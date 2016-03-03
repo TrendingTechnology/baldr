4b37012e6ea0379bdac589c5f8e5b09d8410ba18
Josef Friedrich <josef@friedrich.rocks>
2016-03-03 21:30:46 +0100

    Make build song cache synchronous

--- build-song-cache.js
+++ build-song-cache.js
@@ -2,24 +2,22 @@ var fs = require('fs');
 
 var songs = {};
 
-fs.readdir('./songs', function(err, folders) {
-  folders.forEach(function (folder) {
-    var path = './songs/' + folder + '/'
-    var json = fs.readFileSync(path + 'info.json', 'utf8');
-    var info = JSON.parse(json);
-    var files = fs.readdirSync(path)
-    info.folder = folder;
-    info.files = files;
-    songs[folder] = info;
-  });
+folders = fs.readdirSync('./songs');
 
-  fs.writeFile('songs.json', JSON.stringify(songs, null, 4), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log(songs);
-    }
-  });
+folders.forEach(function (folder) {
+  var path = './songs/' + folder + '/'
+  var json = fs.readFileSync(path + 'info.json', 'utf8');
+  var info = JSON.parse(json);
+  var files = fs.readdirSync(path)
+  info.folder = folder;
+  info.files = files;
+  songs[folder] = info;
 });
+
+var json = JSON.stringify(songs, null, 4)
+
+fs.writeFileSync('songs.json', json);
+
+console.log(json);
 
 
