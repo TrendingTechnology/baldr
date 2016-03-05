fd9fc93c1d9c19c93ef76cc470e0b1ff6bc8219b
Josef Friedrich <josef@friedrich.rocks>
2016-03-05 21:57:17 +0100

    Refactoring: function to objects.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,130 @@
+$(document).ready(function() {
+  songs.setLibrary();
+  SearchBar.generateDatalist();
+  $(document).bind('keydown', 'shortcut', bindShortcuts);
+});
+
+function bindShortcuts(shortcut) {
+
+    switch (shortcut.which) {
+        // ESC
+        case 27:
+            SearchBar.toggle();
+            break;
+        // Cursor left
+        case 37:
+            song.previousSlide();
+            break;
+
+        // Cursor right
+        case 39:
+            song.nextSlide();
+            break;
+    }
+}
+
+
+/***********************************************************************
+ * Object 'songs': All songs in the songs library.
+ **********************************************************************/
+
 /**
- * Contains the songs.json file.
+ *
  */
 var songs = {}
 
 /**
- * The current song to view.
+ *
  */
-var song = {}
-song.slideNumber = 0;
-
-$(document).ready(function() {
-  get_songs();
-  generate_search_list();
-  get_selected_song();
-  hotkey_bindings();
-});
-
-function get_songs() {
-  $.getJSON('songs.json', function(songs) {
-    window.songs = songs;
-  });
+songs.setLibrary = function() {
+  var json = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': 'songs.json',
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+  songs.library = json;
 }
 
-function generate_search_list() {
-  $.getJSON('songs.json', function(songs) {
+
+
+/***********************************************************************
+ * Object 'song': The current song
+ **********************************************************************/
+
+var song = {}
+
+/**
+ * The current slide number.
+ */
+song.slideNumber = 0;
+
+/**
+ * The biggest slide number.
+ */
+song.slideNumberMax;
+
+/**
+ *
+ */
+song.setSlide = function(slideNumber) {
+  if (! slideNumber) {
+    slideNumber = 0
+  }
+
+  if (typeof song.meta.folder != 'undefined') {
+    var path = '/songs/' + song.meta.folder + '/' + song.meta.files[slideNumber];
+    $('#slide img').attr('src', path);
+  }
+}
+
+/**
+ *
+ */
+song.slideNext = function() {
+  song.slideNumber += 1;
+  if (song.slideNumber > max_slide_number) {
+    song.slideNumber = 0
+  }
+}
+
+/**
+ *
+ */
+song.slidePrevious = function() {
+  song.slideNumber -= 1;
+  if (song.slideNumber < 0) {
+    song.slideNumber = max_slide_number
+  }
+}
+
+/***********************************************************************
+ * Object 'search': Search bar
+ **********************************************************************/
+
+var SearchBar = {};
+
+/**
+ * Generate a data list for an text input field containing all songs.
+ */
+SearchBar.generateDatalist = function() {
     // Get the <datalist> and <input> elements.
     var dataList = document.getElementById('songs');
     var input = document.getElementById('song-search');
 
-    for(key in songs){
+    for(key in songs.library){
       // Create a new <option> element.
       var option = document.createElement('option');
 
       // Set the value using the item in the JSON array.
-      option.value = songs[key].title;
+      option.value = songs.library[key].title;
       option.id = key;
       // Add the <option> element to the <datalist>.
       dataList.appendChild(option);
@@ -41,9 +132,25 @@ function generate_search_list() {
 
     // Update the placeholder text.
     input.placeholder = 'e. g.: Freude schöner Götterfunken';
-  });
 }
 
+/**
+ * Hide or show search bar.
+ */
+SearchBar.toggle = function() {
+  var displayState = document.getElementById('search').style.display;
+  console.log(displayState);
+  if (displayState == 'none') {
+    $('#search').show();
+    document.getElementById('song-search').focus();
+  } else {
+    $('#search').hide();
+  }
+}
+
+/**
+ *
+ */
 function get_selected_song() {
   $('#song-search').on('input', function() {
     var song_title = $(this).val();
@@ -58,49 +165,5 @@ function get_selected_song() {
       }
     })
   });
-}
-
-function hide_search_section(shortcut) {
-  // 27 = 'ESC'
-  var displayState = document.getElementById('search').style.display;
-  if (shortcut.which == 27 && (displayState == 'none')) {
-    $('#search').show();
-    document.getElementById('song-search').focus();
-  } else if (shortcut.which == 27) {
-    $('#search').hide();
-  }
-}
-
-function hotkey_bindings() {
-  $(document).bind('keydown', 'shortcut', hide_search_section);
-  $(document).bind('keydown', 'shortcut', slide_songs);
-}
-
-function slide_songs(shortcut) {
-  // 39 -> right
-  // 37 -> left
-
-  var max_slide_number = song.meta.files.length - 1
-
-  if (shortcut.which == 37) {
-    song.slideNumber -= 1;
-    if (song.slideNumber < 0) {
-      song.slideNumber = max_slide_number
-    }
-  } else if (shortcut.which == 39) {
-    song.slideNumber += 1;
-    if (song.slideNumber > max_slide_number) {
-      song.slideNumber = 0
-    }
-  }
-  setSlide(song.slideNumber);
-}
-
-function setSlide(slideNumber) {
-  if (! slideNumber) {
-    slideNumber = 0
-  }
-  var path = '/songs/' + song.meta.folder + '/' + song.meta.files[slideNumber];
-  $('#slide img').attr('src', path);
 }
 
