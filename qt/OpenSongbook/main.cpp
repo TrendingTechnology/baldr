6094bda85782274a88234a11bc1b7e85fd29a66c
Josef Friedrich <josef@friedrich.rocks>
2016-05-15 10:19:55 +0200

    Enable remote debugging

--- qt/OpenSongbook/main.cpp
+++ qt/OpenSongbook/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 
 	QApplication app (argc, argv);
 
-	QUrl url = (argc > 1) ? QUrl::fromUserInput(argv[1]) : QUrl("https://cheat.friedrich.rocks");
+    QUrl url = QUrl("https://cheat.friedrich.rocks");
 	QWebEngineView view;
 	view.load(url);
 	view.setWindowState(view.windowState() ^ Qt::WindowFullScreen);
