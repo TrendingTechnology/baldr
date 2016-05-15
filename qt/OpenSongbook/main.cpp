a2add16f11d0fa75f263d9d7cf83fb92389971d9
Josef Friedrich <josef@friedrich.rocks>
2016-05-15 09:32:33 +0200

    Implement mirroring on MAC os X

--- qt/OpenSongbook/main.cpp
+++ qt/OpenSongbook/main.cpp
@@ -1,10 +1,16 @@
 #include <QtWebEngineWidgets>
+#include <QProcess>
 
 int main(int argc, char **argv)
 {
-	QApplication app (argc, argv);
-	QUrl url = (argc > 1) ? QUrl::fromUserInput(argv[1]) : QUrl("https://cheat.friedrich.rocks");
+	#ifdef Q_OS_MAC
+		QProcess mirror;
+		mirror.start("/usr/local/bin/mirror", QStringList() << "-on");
+	#endif
 
+	QApplication app (argc, argv);
+
+	QUrl url = (argc > 1) ? QUrl::fromUserInput(argv[1]) : QUrl("https://cheat.friedrich.rocks");
 	QWebEngineView view;
 	view.load(url);
 	view.setWindowState(view.windowState() ^ Qt::WindowFullScreen);
