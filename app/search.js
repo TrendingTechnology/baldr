7ef484cc63d9a67b8c7301dd9835e4934a014d9a
Josef Friedrich <josef@friedrich.rocks>
2016-06-02 09:01:36 +0200

    Split search code in separate modul

---
+++ app/search.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var pth = require('path');
+
+build = function() {
+  document.getElementById('field').appendChild(makeList(songs.library));
+}
+
+makeList = function(library) {
+  var select = document.createElement('select');
+  select.setAttribute('id', 'select');
+  select.setAttribute('placeholder', 'Suche nach einem Lied');
+
+  var option = document.createElement('option');
+  option.setAttribute('value', '');
+  select.appendChild(option);
+
+  for (songID in library) {
+    var option = document.createElement('option');
+    option.setAttribute('value', songID);
+    option.innerHTML = library[songID].title;
+    select.appendChild(option);
+  }
+  return select;
+}
+
+resetSelect = function() {
+  var select = document.getElementById('select');
+  var option = document.createElement('option');
+  option.setAttribute('value', '');
+  select.insertBefore(option, select.firstChild);
+}
+
+songs.setLibrary();
+bindShortcuts();
+
+$(function() {
+    var selectized = $('select').selectize({
+      onItemAdd: function(value, data) {
+        song.setCurrent(value);
+        song.setSlide();
+        modal.hide();
+      }
+    });
+    search.selectize = selectized[0].selectize;
+    search.selectize.focus();
+});
