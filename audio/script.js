340c123ff7d4de1916f0af017803df47cdcd5846
Josef Friedrich <josef@friedrich.rocks>
2017-02-12 12:14:19 +0100

    Test howler

--- audio/script.js
+++ audio/script.js
@@ -1,5 +1,15 @@
 var sound = new Howl({
-  src: ['sample.mp3']
+  src: ['sample.mp3'],
+	html5: true,
+	sprite: {
+		blast: [0, 3000],
+		laser: [4000, 500],
+		winner: [6000, 5000]
+	}
 });
 
 Mousetrap.bind('p', function() {sound.play();})
+Mousetrap.bind('l', function() {sound.play('laser');})
+Mousetrap.bind('b', function() {sound.play('blast');})
+Mousetrap.bind('w', function() {sound.play('winner');})
+Mousetrap.bind('f', function() {sound.fade(1, 0, 5000);})
