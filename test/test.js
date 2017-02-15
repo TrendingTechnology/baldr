1c1af714658be3d230e0e63f831b7ef79c01d972
Josef Friedrich <josef@friedrich.rocks>
2017-02-15 08:32:13 +0100

    Add more tests

--- test/test.js
+++ test/test.js
@@ -3,12 +3,25 @@ const assert = require('assert');
 var rewire = require("rewire");
 var slu = rewire("../index.js");
 
-var config = slu.__get__("configDefault");
+describe('Configuration', function() {
+  var config = slu.__get__("configDefault");
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+  describe('default configuration', function() {
+    it('"config.json" should return "songs.json"', function() {
+      assert.equal(config.json, "songs.json");
+    });
+    it('"config.info" should return "info.json"', function() {
+      assert.equal(config.info, "info.json");
+    });
+    it('"config.mtime" should return ".mtime"', function() {
       assert.equal(config.mtime, ".mtime");
     });
+  });
+});
+
+describe('Functions', function() {
+  var getMscoreCommand = slu.__get__("getMscoreCommand");
+  it('"getMscoreCommand()" should return "mscore"', function() {
+    assert.equal(getMscoreCommand(), "mscore");
   });
 });
