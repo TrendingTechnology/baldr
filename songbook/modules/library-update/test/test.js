70789b114c1d7d2c1dd4760ad4c5638ad03ddc8f
Josef Friedrich <josef@friedrich.rocks>
2017-09-24 17:32:18 +0200

    Use hashes instead of timestamps

--- songbook/modules/library-update/test/test.js
+++ songbook/modules/library-update/test/test.js
@@ -14,6 +14,16 @@ slu.bootstrapConfig({
   force: true,
 });
 
+var rewireScript = function() {
+  script = rewire('../index.js');
+  script.bootstrapConfig({
+    test: true,
+    path: path.resolve('songs'),
+    force: true,
+  });
+  return script;
+}
+
 exists = function() {
   assert.ok(fs.existsSync(path.join.apply(null, arguments)));
 };
@@ -50,7 +60,7 @@ describe('Configuration', function() {
     const c = s.__get__('config');
     assert.equal(c.path, path.resolve('songs'));
     assert.equal(c.json, 'songs.json');
-    exists(path.resolve('songs', 'filestats.db'))
+    exists(path.resolve('songs', 'filehashes.db'))
   });
 });
 
@@ -85,30 +95,37 @@ describe('Private functions', function() {
 
   describe('"fileChanged()"', function() {
     it('"fileChanged()": run once', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       fs.appendFileSync('tmp.txt', 'test');
       assert.ok(fileChanged('tmp.txt'));
       fs.unlinkSync('tmp.txt');
+      slu.clean();
     });
 
     it('"fileChanged()": run twice', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       fs.appendFileSync('tmp.txt', 'test');
       assert.ok(fileChanged('tmp.txt'));
       assert.ok(!fileChanged('tmp.txt'));
       fs.unlinkSync('tmp.txt');
+      slu.clean();
     });
 
     it('"fileChanged()": run three times', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       fs.appendFileSync('tmp.txt', 'test');
       assert.ok(fileChanged('tmp.txt'));
       assert.ok(!fileChanged('tmp.txt'));
       assert.ok(!fileChanged('tmp.txt'));
       fs.unlinkSync('tmp.txt');
+      slu.clean();
     });
 
     it('"fileChanged()": change file', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       fs.appendFileSync('tmp.txt', 'test');
       assert.ok(fileChanged('tmp.txt'));
@@ -116,29 +133,36 @@ describe('Private functions', function() {
       fs.appendFileSync('tmp.txt', 'test');
       assert.ok(fileChanged('tmp.txt'));
       fs.unlinkSync('tmp.txt');
+      slu.clean();
     });
 
     it('"fileChanged()": nonexisting file', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       assert.ok(!fileChanged('tmxxxxxxxxxxxxxxxxxxxxxxp.txt'));
+      slu.clean();
     });
 
     it('"fileChanged()": in folder', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       var tmp = p('songs', 'tmp.txt')
       fs.appendFileSync(tmp, 'test');
       assert.ok(fileChanged(tmp));
       assert.ok(!fileChanged(tmp));
       fs.unlinkSync(tmp);
+      slu.clean();
     });
 
     it('"fileChanged()": absolute path', function() {
+      var slu = rewireScript();
       var fileChanged = slu.__get__('fileChanged');
       var tmp = path.resolve('tmp.txt')
       fs.appendFileSync(tmp, 'test');
       assert.ok(fileChanged(tmp));
       assert.ok(!fileChanged(tmp));
       fs.unlinkSync(tmp);
+      slu.clean();
     });
   });
 
