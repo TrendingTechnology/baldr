5723041cbc9da372be618eaa455533191fb11cfd
Josef Friedrich <josef@friedrich.rocks>
2017-09-30 10:15:30 +0200

    Clean up tests

---
+++ test/cli.js
@@ -0,0 +1,58 @@
+/* jshint esversion: 6 */
+
+const {assert} = require('./lib/helper.js');
+const spawn = require('child_process').spawnSync;
+
+before(() => {
+  process.env.PATH = __dirname + '/bin:' + process.env.PATH;
+});
+
+describe('Command line', () => {
+
+  it('no arguments: normal update', function() {
+    const cli = spawn('./command.js', ['--test']);
+  });
+
+  it('no arguments (second run): only json and TeX generation', function() {
+    const cli = spawn('./command.js', ['--test']);
+  });
+
+  it('--force', function() {
+    const cli = spawn('./command.js', ['--test', '--force']);
+  });
+
+  // After --force
+  it('--json', function() {
+    const cli = spawn('./command.js', ['--test', '--json']);
+  });
+
+  // After --force
+  it.skip('--tex', function() {
+    const cli = spawn('./command.js', ['--test', '--tex']);
+  });
+
+  it.skip('--folder', function() {
+    const cli = spawn('./command.js', ['--test', '--folder', 'Swing-low']);
+    assert.exists('Swing-low', 'piano', 'piano.eps');
+  });
+
+  it('--help', function() {
+    const cli = spawn('./command.js', ['--test', '--help']);
+    var out = cli.stdout.toString();
+    assert.ok(out.indexOf('Usage') > -1);
+    assert.ok(out.indexOf('--help') > -1);
+    assert.ok(out.indexOf('--version') > -1);
+    assert.equal(cli.status, 0);
+  });
+
+  it('--version', function() {
+    const cli = spawn('./command.js', ['--test', '--version']);
+    assert.equal(cli.stdout.toString(), '0.0.5\n');
+    assert.equal(cli.status, 0);
+  });
+
+  // Test should be executed at the very last position.
+  it('--clean', function() {
+    const cli = spawn('./command.js', ['--test', '--clean']);
+  });
+});
