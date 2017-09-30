3fbc79dad885f94bb28865ac925fb26814cf715f
Josef Friedrich <josef@friedrich.rocks>
2017-09-30 12:21:29 +0200

    Better folder structure in test

--- test/folder-tree.js
+++ test/folder-tree.js
@@ -19,27 +19,27 @@ describe('folder-tree', () => {
 
     it('"getFolderFiles()": eps', () => {
       const files = tree.getFolderFiles(
-        path.join('test', 'piano-files'), '.eps'
+        path.join('test', 'files', 'piano'), '.eps'
       );
       assert.deepEqual(files, ['01.eps', '02.eps', '03.eps']);
     });
 
     it('"getFolderFiles()": svg', () => {
       const files = tree.getFolderFiles(
-        path.join('test', 'slides-files'), '.svg'
+        path.join('test', 'files', 'slides'), '.svg'
       );
       assert.deepEqual(files, ['01.svg', '02.svg', '03.svg']);
     });
 
     it('"getFolderFiles()": non existent folder', () => {
       const files = tree.getFolderFiles(
-        path.join('test', 'lol'), '.svg'
+        path.join('test', 'files', 'lol'), '.svg'
       );
       assert.deepEqual(files, []);
     });
 
     it('"getFolderFiles()": empty folder', () => {
-      const empty = path.join('test', 'empty');
+      const empty = path.join('test', 'files', 'empty');
       fs.mkdirSync(empty);
       const files = tree.getFolderFiles(
         empty, '.svg'
