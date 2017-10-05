94edd6928e96592c7c81190a32d40c5ea66b7e3f
Josef Friedrich <josef@friedrich.rocks>
2017-10-05 10:36:59 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -17,92 +17,108 @@ const rewire = require('rewire')('../index.js');
 
 process.env.PATH = __dirname + '/bin:' + process.env.PATH;
 
-/**
- *
- */
-describe('Configuration', () => {
-  const config = rewireBootstrapped.__get__('config');
+describe('index.js', () => {
 
-  describe('default configuration', () => {
-    it('"config.json" should return "songs.json"', () => {
-      assert.equal(config.json, 'songs.json');
-    });
-    it('"config.info" should return "info.json"', () => {
-      assert.equal(config.info, 'info.json');
-    });
-  });
-
-  it('"bootstrapConfig()"', () => {
-    rewire.bootstrapConfig({path: path.resolve('songs'), test: true});
-    const c = rewire.__get__('config');
-    assert.equal(c.path, path.resolve('songs'));
-    assert.equal(c.json, 'songs.json');
-    assert.exists(path.resolve('songs', 'filehashes.db'));
-  });
-
-  it('"bootstrapConfig()": exit', () => {
-    let savePATH = process.env.PATH;
-    process.env.PATH = '';
-    try {
-      rewire.bootstrapConfig({path: path.resolve('songs'), test: true});
-    }
-    catch(e) {
-      assert.equal(
-        e.message,
-        'Some dependencies are not installed: “mscore-to-eps.sh”, ' +
-        '“pdf2svg”, “pdfcrop”, “pdfinfo”, “pdftops”, “mscore”'
-      );
-      assert.equal(e.name, 'UnavailableCommandsError');
-    }
-    process.env.PATH = savePATH;
-  });
-
-});
-
-/**
- *
- */
-describe('Exported functions', () => {
-
-  it('"update()"', () => {
-    rewireBootstrapped.update();
-    var config = rewireBootstrapped.__get__('config');
-    const auf = path.join('songs', 'a', 'Auf-der-Mauer_auf-der-Lauer');
-    const swing = path.join('songs', 's', 'Swing-low');
-    const zum = path.join('songs', 'z', 'Zum-Tanze-da-geht-ein-Maedel');
-    const folders = [auf, swing, zum];
-
-    for (i = 0; i < folders.length; ++i) {
-      assert.exists(folders[i], config.slidesFolder);
-      assert.exists(folders[i], config.slidesFolder, '01.svg');
-      assert.exists(folders[i], config.pianoFolder);
-      assert.exists(folders[i], config.pianoFolder, 'piano.mscx');
-    }
-
-    assert.exists(auf, config.pianoFolder, 'piano_1.eps');
-    assert.exists(swing, config.pianoFolder, 'piano_1.eps');
-    assert.exists(zum, config.pianoFolder, 'piano_1.eps');
-    assert.exists(zum, config.pianoFolder, 'piano_2.eps');
-
-    var info = JSON.parse(fs.readFileSync(path.join(config.path, 'songs.json'), 'utf8'));
-    assert.equal(
-      info.a['Auf-der-Mauer_auf-der-Lauer'].title,
-      'Auf der Mauer, auf der Lauer'
-    );
-
-    rewireBootstrapped.clean();
-  });
-
-  it('"setTestMode()"', () => {
-    rewireBootstrapped.setTestMode();
+  /**
+   *
+   */
+  describe('Configuration', () => {
     const config = rewireBootstrapped.__get__('config');
-    assert.equal(config.test, true);
-    assert.equal(config.path, path.resolve('./songs'));
+
+    describe('default configuration', () => {
+      
+      it('"config.json" should return "songs.json"', () => {
+        assert.equal(config.json, 'songs.json');
+      });
+
+      it('"config.info" should return "info.json"', () => {
+        assert.equal(config.info, 'info.json');
+      });
+    });
+
+    it('"bootstrapConfig()"', () => {
+      rewire.bootstrapConfig({path: path.resolve('songs'), test: true});
+      const c = rewire.__get__('config');
+      assert.equal(c.path, path.resolve('songs'));
+      assert.equal(c.json, 'songs.json');
+      assert.exists(path.resolve('songs', 'filehashes.db'));
+    });
+
+    it('"bootstrapConfig()": exit', () => {
+      let savePATH = process.env.PATH;
+      process.env.PATH = '';
+      try {
+        rewire.bootstrapConfig({path: path.resolve('songs'), test: true});
+      }
+      catch(e) {
+        assert.equal(
+          e.message,
+          'Some dependencies are not installed: “mscore-to-eps.sh”, ' +
+          '“pdf2svg”, “pdfcrop”, “pdfinfo”, “pdftops”, “mscore”'
+        );
+        assert.equal(e.name, 'UnavailableCommandsError');
+      }
+      process.env.PATH = savePATH;
+    });
+
   });
 
-  it('"clean()"', () => {
-    rewireBootstrapped.clean();
-    assert.ok(!fs.existsSync(path.join('songs', 'songs.tex')));
+  describe('Private functions', () => {
+    if('“processSongFolder()”', () => {
+      processSongFolder = rewireBootstrapped.__get__("processSongFolder");
+      let stat = processSongFolder(
+        path.join('songs', 'a', 'Auf-der-Mauer_auf-der-Lauer')
+      );
+      assert.deepEqual(stat, {});
+    });
+  });
+
+  /**
+   *
+   */
+  describe('Exported functions', () => {
+
+    it('"update()"', () => {
+      rewireBootstrapped.update();
+      var config = rewireBootstrapped.__get__('config');
+      const auf = path.join('songs', 'a', 'Auf-der-Mauer_auf-der-Lauer');
+      const swing = path.join('songs', 's', 'Swing-low');
+      const zum = path.join('songs', 'z', 'Zum-Tanze-da-geht-ein-Maedel');
+      const folders = [auf, swing, zum];
+
+      for (i = 0; i < folders.length; ++i) {
+        assert.exists(folders[i], config.slidesFolder);
+        assert.exists(folders[i], config.slidesFolder, '01.svg');
+        assert.exists(folders[i], config.pianoFolder);
+        assert.exists(folders[i], config.pianoFolder, 'piano.mscx');
+      }
+
+      assert.exists(auf, config.pianoFolder, 'piano_1.eps');
+      assert.exists(swing, config.pianoFolder, 'piano_1.eps');
+      assert.exists(zum, config.pianoFolder, 'piano_1.eps');
+      assert.exists(zum, config.pianoFolder, 'piano_2.eps');
+
+      var info = JSON.parse(fs.readFileSync(path.join(config.path, 'songs.json'), 'utf8'));
+      assert.equal(
+        info.a['Auf-der-Mauer_auf-der-Lauer'].title,
+        'Auf der Mauer, auf der Lauer'
+      );
+
+      rewireBootstrapped.clean();
+    });
+
+    it('"setTestMode()"', () => {
+      rewireBootstrapped.setTestMode();
+      const config = rewireBootstrapped.__get__('config');
+      assert.equal(config.test, true);
+      assert.equal(config.path, path.resolve('./songs'));
+    });
+
+    it('"clean()"', () => {
+      rewireBootstrapped.clean();
+      assert.ok(!fs.existsSync(path.join('songs', 'songs.tex')));
+    });
+
   });
 
 });
