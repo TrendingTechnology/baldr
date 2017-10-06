14dee90f556bcd62a473cd1025b612a313deae55
Josef Friedrich <josef@friedrich.rocks>
2017-10-06 20:29:48 +0200

    Add argument for setOptions function

--- songbook/modules/library-update/command.js
+++ songbook/modules/library-update/command.js
@@ -14,20 +14,7 @@ try {
   var index = require('./index.js');
 } catch (e) {}
 
-var setOptions = function() {
-  return commander
-    .version('0.0.5')
-    .option('-c, --clean', 'clean up (delete all generated files)')
-    .option('-F, --folder <folder>', 'process only the given song folder')
-    .option('-f, --force', 'rebuild all images')
-    .option('-j, --json', 'generate JSON file')
-    .option('-p --path <path>', 'Base path to a song collection.')
-    .option('-T, --test', 'switch to test mode')
-    .option('-t, --tex', 'generate TeX file')
-    .parse(process.argv);
-};
-
-var setOpt = function(argv) {
+var setOptions = function(argv) {
   return commander
     .version('0.0.5')
     .option('-c, --clean', 'clean up (delete all generated files)')
@@ -74,7 +61,7 @@ var processOptions =  function(options) {
 };
 
 var main = function() {
-  processOptions(setOptions());
+  processOptions(setOptions(process.argv));
 };
 
 if (require.main === module) {
