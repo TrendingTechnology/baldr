6a8e8d76c59e45c0cfe8906e173765bae30415ce
Josef Friedrich <josef@friedrich.rocks>
2017-10-11 13:42:55 +0200

    Add first spectron test

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 const fs = require('fs');
-
+var Application = require('spectron').Application
 
 describe('build', () => {
 
@@ -9,3 +9,26 @@ describe('build', () => {
   });
 
 });
+
+describe('application launch', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: 'dist/baldr-sbook-linux-x64/baldr-sbook'
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1)
+    })
+  })
+})
