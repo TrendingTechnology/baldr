7a7c31f93b5fb3aefb72a2b41e985adf4f59cd30
Josef Friedrich <josef@friedrich.rocks>
2017-10-16 15:53:13 +0200

    Add docs

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+/**
+ * @file Index file of the render process.
+ */
+
 const yaml = require('js-yaml');
 const fs = require('fs');
 
+/**
+ * Load the content of a yaml file and convert its content into a
+ * object.
+ * @param {string} yamlFile Path of the yaml file.
+ * @return {object} Object representation of the yaml file.
+ */
 var loadYaml = function(yamlFile) {
   try {
     return yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
