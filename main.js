bfe4e0fac74343aa003b22297c3273491c911004
Josef Friedrich <josef@friedrich.rocks>
2017-10-23 10:36:35 +0200

    Fix test

--- main.js
+++ main.js
@@ -115,7 +115,6 @@ function createWindow() {
   });
 }
 
-
 // https://blog.dcpos.ch/how-to-make-your-electron-app-sexy
 // In the main process, check whether the app is starting
 // because the user dragged files onto the app icon
@@ -125,8 +124,8 @@ function createWindow() {
 // These fire if the app is already running and the user
 // drags files or URLs onto the dock icon, or if they set
 // the app as a handler for a file type and then open a file
-app.on('open-file', onOpen)
-app.on('open-url', onOpen)
+//app.on('open-file', onOpen);
+//app.on('open-url', onOpen);
 
 // Separately, in the renderer process, you can use the HTML5
 // drag-drop API to create a drop target and handle files
