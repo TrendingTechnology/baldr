9ee23c36485c5bf45166937eccc8984c23b9a652
Josef Friedrich <josef@friedrich.rocks>
2017-10-23 19:09:47 +0200

    Add example file

--- masters/quote/index.js
+++ masters/quote/index.js
@@ -3,8 +3,6 @@ const path = require('path');
 
 const compiledFunction = pug.compileFile(path.join(__dirname, 'template.pug'));
 
-var render = function(data) {
+exports.render = function(data) {
   return compiledFunction(data);
 };
-
-exports.render = render;
