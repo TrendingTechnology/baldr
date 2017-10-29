1d6177712e58ea93408c0d15c6a8850d29852c2e
Josef Friedrich <josef@friedrich.rocks>
2017-10-29 08:30:40 +0100

    Implement MasterOfMasters class

--- test/lib-masters.js
+++ test/lib-masters.js
@@ -1,10 +1,19 @@
 const assert = require('assert');
 const path = require('path');
+const fs = require('fs');
 const Masters = require('../lib/masters.js').Masters;
+const MasterOfMasters = require('../lib/masters.js').MasterOfMasters;
 const masters = new Masters();
 const rewire = require('rewire')('../lib/masters.js');
 Master = rewire.__get__('Master');
 const master = new Master('quote');
+
+const {JSDOM} = require('jsdom');
+
+function getDOM(html) {
+  let d = new JSDOM(html);
+  return d.window.document;
+}
 
 let all = [
   'audio',
@@ -86,4 +95,37 @@ describe('Class “Masters()”', () => {
     assert.deepEqual(masters.getModules(), all);
   });
 
+});
+
+describe('Class “MasterOfMasters”', function() {
+  beforeEach(function() {
+
+    this.mom = new MasterOfMasters(
+      getDOM(
+        fs.readFileSync(
+          path.join(__dirname, '..', 'render.html'),
+          'utf8'
+        )
+      )
+    );
+  });
+
+  it('Instantiation', function() {
+    assert.equal(typeof MasterOfMasters, 'function');
+    assert.equal(typeof this.mom, 'object');
+  });
+
+  it('Property “document”', function() {
+    assert.equal(typeof this.mom.document, 'object');
+  });
+
+  it('Property “elemSlide”', function() {
+    assert.equal(this.mom.elemSlide.id, 'slide');
+    assert.equal(this.mom.elemSlide.nodeName, 'DIV');
+    assert.equal(this.mom.elemSlide.nodeType, 1);
+  });
+
+  it('Method hasCSS()', function() {
+    assert.equal(typeof this.mom.hasCSS, 'function');
+  });
 });
