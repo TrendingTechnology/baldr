32824cf00ae873aa03fd397712fc5425ea59de5a
Josef Friedrich <josef@friedrich.rocks>
2017-10-30 23:28:34 +0100

    Fix instantiateSlide

--- test/lib-slides.js
+++ test/lib-slides.js
@@ -1,10 +1,35 @@
 const assert = require('assert');
 const path = require('path');
+const fs = require('fs');
 
 const rewire = require('rewire')('../lib/slides.js');
 const {Slides} = require('../lib/slides.js');
 
 const minimal = path.join('test', 'files', 'minimal.baldr');
+
+const {JSDOM} = require('jsdom');
+
+function getDOM(html) {
+  let d = new JSDOM(html);
+  return d.window.document;
+}
+
+let all = [
+  'audio',
+  'camera',
+  'editor',
+  'person',
+  'question',
+  'quote',
+  'svg'
+];
+
+let document = getDOM(
+  fs.readFileSync(
+    path.join(__dirname, '..', 'render.html'),
+    'utf8'
+  )
+);
 
 describe('Class “Slides()”', () => {
 
@@ -99,6 +124,22 @@ describe('Class “Slides()”', () => {
 
     let slides = new Slides(minimal);
     assert.deepEqual(slides.parseSlides(rawYaml), result);
+  });
+
+  it('Method “instantiateSlides()”', () => {
+    let rawSlideObj = {
+        "quote": {
+          "text": "text",
+          "author": "author",
+          "date": "date"
+        }
+      };
+
+    let slides = new Slides(minimal, document);
+    let slide = slides.instantiateSlide(rawSlideObj, 0);
+    assert.equal(slide.slideNo, 1);
+    assert.equal(slide.masterName, 'quote');
+    assert.equal(typeof slide.render, 'function');
   });
 
   it('Method “parse()”', () => {
