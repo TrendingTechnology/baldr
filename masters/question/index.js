cae025f86a2c8e3e71798645249dfa391b165848
Josef Friedrich <josef@friedrich.rocks>
2017-11-02 11:16:08 +0100

    Add some work in progress code for master slide “question”

--- masters/question/index.js
+++ masters/question/index.js
@@ -22,21 +22,64 @@ class MasterQuestion extends MasterOfMasters {
 
   constructor(propObj) {
     super(propObj);
+  }
 
-
+  /**
+   *
+   */
+  normalizeDataQAPair(pair) {
+    if (typeof pair === 'string') {
+      return {question: pair, answer: false};
+    }
+    else if (typeof pair === 'object') {
+      if (typeof pair.question === 'string' && !pair.answer) {
+        return {question: pair.question, answer: false};
+      }
+      else if (typeof pair.question === 'string' && typeof pair.answer === 'string') {
+        return {question: pair.question, answer: pair.answer};
+      }
+      else {
+        throw new Error('Master slide “question”: Invalid data input');
+      }
+    }
+    else {
+      throw new Error('Master slide “question”: Invalid data input');
+    }
   }
 
   /**
    *
    */
   normalizeData(data) {
-    return `<li>${question}: ${answer}</li>`;
+     if (typeof data === 'object' && Array.isArray(data)) {
+      let out = [];
+      for (let pair of data) {
+        out.push(this.normalizeDataQAPair(pair));
+      }
+      return out;
+    } else {
+      return [this.normalizeDataQAPair(data)];
+    }
   }
 
   /**
    *
    */
-  renderQuestionAnswerPair(question, answer) {
+  templatQAPair(question, answer) {
+    return '';
+  }
+
+  /**
+   *
+   */
+  template(normalizedData) {
+    return '`<li>${question}: ${answer}</li>`';
+  }
+
+  /**
+   *
+   */
+  renderQuestion(question, answer) {
     return `<li>${question}: ${answer}</li>`;
   }
 
--- masters/question/test/index.js
+++ masters/question/test/index.js
@@ -27,19 +27,59 @@ let setHTMLSlide = function(data) {
   return quote.setHTMLSlide();
 };
 
-let dataOne = 'One big question?';
-let dataMultiple = ['One?', 'Two?', 'Three?'];
+let dataSingleWithout = 'One?';
+let dataSingleWithAnswer = {question: 'One?', answer: 'One'};
 
-describe('Master slide “quote”', () => {
+let dataMultipleWithout = ['One?', 'Two?'];
+let dataMultipleWithAnswer = [
+  {question: 'One?', answer: 'One'},
+  {question: 'Two?', answer: 'Two'}
+];
 
-  it('function “setHTMLSlide()”: all values', () => {
-    let html = render({
-      text: 'text',
-      author: 'author',
-      date: 'date'
-    });
+describe('Master slide “question”', () => {
 
+  it('function “normalizeData()”', () => {
+    let question = getQuestion('');
 
+    assert.deepEqual(
+      question.normalizeData(dataSingleWithout),
+      [{question: 'One?', answer: false}]
+    );
+
+    assert.deepEqual(
+      question.normalizeData(dataSingleWithAnswer),
+      [{question: 'One?', answer: 'One'}]
+    );
+
+    assert.deepEqual(
+      question.normalizeData(dataMultipleWithout),
+      [
+        {question: 'One?', answer: false},
+        {question: 'Two?', answer: false}
+      ]
+    );
+
+    assert.deepEqual(
+      question.normalizeData(dataMultipleWithAnswer),
+      [
+        {question: 'One?', answer: 'One'},
+        {question: 'Two?', answer: 'Two'}
+      ]
+    );
+
+    // let invalidData = function(data) {
+    //   try {
+    //     question.normalizeData(data);
+    //   }
+    //   catch(error) {
+    //     assert.equal(error.message, 'Master slide “question”: Invalid data input');
+    //     throw error;
+    //   }
+    // }
+    //
+    // invalidData(false);
+    // invalidData(true);
+    // invalidData({lol: 'lol', troll: 'troll'});
   });
 
 });
