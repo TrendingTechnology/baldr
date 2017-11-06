62bc9d453fa3b269eef10a92d870bdc342147ea8
Josef Friedrich <josef@friedrich.rocks>
2017-11-06 16:52:40 +0100

    Add more spectron tests

--- masters/question/test/spectron.js
+++ masters/question/test/spectron.js
@@ -18,10 +18,30 @@ describe('question example.baldr', function () {
     return this.spectron.stop();
   });
 
-  it('Initial window', function () {
+  it('Text on the example slides', function () {
     return this.app.client
       .getText('li:nth-child(1) .question')
-      .then(text => {assert.equal(text, 'Question one?');});
+      .then(text => {assert.equal(text, 'Question one?');})
+
+      .click('#nav-slide-next')
+      .getText('.question')
+      .then(text => {assert.equal(text, 'This is a question?');})
+      .getCssProperty('.answer', 'visibility')
+      .then(style => {assert.equal(style.value, 'hidden');})
+
+      .click('#nav-slide-next')
+      .getText('li:nth-child(1) .question')
+      .then(text => {assert.equal(text, 'Question one?');})
+      .getCssProperty('li:nth-child(2) .question', 'visibility')
+      .then(style => {assert.equal(style.value, 'hidden');})
+      .getCssProperty('li:nth-child(3) .question', 'visibility')
+      .then(style => {assert.equal(style.value, 'hidden');})
+
+      .click('#nav-slide-next')
+      .getText('.question')
+      .then(text => {assert.equal(text, 'One big question?');})
+
+      ;
   });
 
 });
