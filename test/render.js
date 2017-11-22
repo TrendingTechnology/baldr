e833701c54972440d4382ec04fa6d3da973b7186
Josef Friedrich <josef@friedrich.rocks>
2017-11-22 09:59:40 +0100

    Test error Page

--- test/render.js
+++ test/render.js
@@ -1,12 +1,20 @@
 const {
   assert,
-  document
+  returnDOM
 } = require('baldr-test');
 
+let rewire;
+let document;
+
 describe('“render.js”', function() {
-  it('“toggleModal()”', function() {
-    const rewire = require('rewire')('../render.js');
+
+  beforeEach(() => {
+    rewire = require('rewire')('../render.js');
+    document = returnDOM();
     rewire.__set__('document', document);
+  });
+
+  it('“toggleModal()”', () => {
     let toggleModal = rewire.__get__('toggleModal');
     let modal = document.getElementById('modal');
 
@@ -18,5 +26,21 @@ describe('“render.js”', function() {
 
     assert.equal(toggleModal(), 'block');
     assert.equal(modal.style.display, 'block');
+  });
+
+  it('“errorPage()”', () => {
+    let errorPage = rewire.__get__('errorPage');
+    let error = {
+      stack: 'stack',
+    };
+    errorPage('message', 'source', 'lineNo', 'colNo', error);
+    let getText = function(selector) {
+      return document.querySelector(selector).textContent
+    };
+    assert.equal(getText('#slide p:nth-child(1)'), 'message');
+    assert.equal(getText('#slide p:nth-child(2)'), 'Source: source');
+    assert.equal(getText('#slide p:nth-child(3)'), 'Line number: lineNo');
+    assert.equal(getText('#slide p:nth-child(4)'), 'Column number: colNo');
+    assert.equal(getText('#slide pre'), 'stack');
   });
 });
