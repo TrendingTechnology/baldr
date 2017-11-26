2797a22029608404e023c0e67d09a7d9e37f4c1b
Josef Friedrich <josef@friedrich.rocks>
2017-11-26 19:57:48 +0100

    Add test for master “person”

--- masters/person/test/index.js
+++ masters/person/test/index.js
@@ -10,16 +10,38 @@ const person = require('../index.js');
 let config = cloneConfig();
 config.sessionDir = path.resolve(__dirname, '..');
 
-let render = function(data) {
+let normalizeData = function(data) {
+  return person.normalizeData(data, config);
+};
+
+let mainHTML = function(data) {
   normalizedData = person.normalizeData(data, config);
   return person.mainHTML({normalizedData: normalizedData}, config);
 };
 
 describe('Master slide “person”: unit tests', () => {
 
-  it('function “hookSetHTMLSlide()”', () => {
 
-    let html = render({
+  it('function “normalizeData()”', () => {
+    const data = normalizeData({
+      name: 'Ludwig van Beethoven',
+      image: 'beethoven.jpg',
+      birth: 1770,
+      death: 1827
+    });
+
+    assert.equal(data.name, 'Ludwig van Beethoven');
+    assert.ok(data.imagePath.includes('beethoven.jpg'));
+    assert.ok(data.imagePath.includes(path.sep));
+    assert.equal(data.birth, '* 1770');
+    assert.equal(data.death, '† 1827');
+    assert.equal(data.birthAndDeath, true);
+  });
+
+
+  it('function “mainHTML()”', () => {
+
+    let html = mainHTML({
       name: 'Ludwig van Beethoven',
       image: 'beethoven.jpg'
     });
