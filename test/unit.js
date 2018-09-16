f298e7188735d3d15f7581f4a222618744860e3b
Josef Friedrich <josef@friedrich.rocks>
2018-09-16 23:38:27 +0200

    Implement more getter

--- test/unit.js
+++ test/unit.js
@@ -6,13 +6,48 @@ const assert = require("assert");
 
 describe('Class Song', () => {
 
-  it('getter title', () => {
-    song = new Song({'title': 'lol'});
-    assert.equal(song.title, 'lol');
-
-    song = new Song({'title': 'lol', 'year': 1984});
-    assert.equal(song.title, 'lol (1984)');
+  describe('get title', () => {
+    it('title only', () => {
+      song = new Song({'title': 'lol'});
+      assert.equal(song.title, 'lol');
+    });
+    it('title and year', () => {
+      song = new Song({'title': 'lol', 'year': 1984});
+      assert.equal(song.title, 'lol (1984)');
+    });
   });
 
+  describe('get subtitle', () => {
+    it('all properties', () => {
+      song = new Song({'subtitle': 's', 'alias': 'a', 'country': 'c'});
+      assert.equal(song.subtitle, 's - a - c');
+    });
+    it('no properties', () => {
+      song = new Song({});
+      assert.equal(song.subtitle, '');
+    });
+  });
+
+  describe('get composer', () => {
+    it('all properties', () => {
+      song = new Song({'composer': 'c', 'artist': 'a', 'genre': 'g'});
+      assert.equal(song.composer, 'c, a, g');
+    });
+    it('no properties', () => {
+      song = new Song({});
+      assert.equal(song.composer, '');
+    });
+  });
+
+  describe('get lyricist', () => {
+    it('all properties', () => {
+      song = new Song({'lyricist': 'l'});
+      assert.equal(song.lyricist, 'l');
+    });
+    it('no properties', () => {
+      song = new Song({});
+      assert.equal(song.lyricist, '');
+    });
+  });
 
 });
