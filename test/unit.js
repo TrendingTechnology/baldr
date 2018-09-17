e432fcf6d3e9745ef90acafa1fbed42854e039db
Josef Friedrich <josef@friedrich.rocks>
2018-09-17 09:35:26 +0200

    Rename class Song to SongMetaData

--- test/unit.js
+++ test/unit.js
@@ -1,51 +1,51 @@
 var rewire = require('rewire');
 var index = rewire("../lib/song-ng.js");
-let Song = index.__get__('Song');
+let SongMetaData = index.__get__('SongMetaData');
 
 const assert = require("assert");
 
-describe('Class Song', () => {
+describe('Class “SongMetaData”', () => {
 
   describe('get title', () => {
     it('title only', () => {
-      song = new Song({'title': 'lol'});
+      song = new SongMetaData({'title': 'lol'});
       assert.equal(song.title, 'lol');
     });
     it('title and year', () => {
-      song = new Song({'title': 'lol', 'year': 1984});
+      song = new SongMetaData({'title': 'lol', 'year': 1984});
       assert.equal(song.title, 'lol (1984)');
     });
   });
 
   describe('get subtitle', () => {
     it('all properties', () => {
-      song = new Song({'subtitle': 's', 'alias': 'a', 'country': 'c'});
+      song = new SongMetaData({'subtitle': 's', 'alias': 'a', 'country': 'c'});
       assert.equal(song.subtitle, 's - a - c');
     });
     it('no properties', () => {
-      song = new Song({});
+      song = new SongMetaData({});
       assert.equal(song.subtitle, '');
     });
   });
 
   describe('get composer', () => {
     it('all properties', () => {
-      song = new Song({'composer': 'c', 'artist': 'a', 'genre': 'g'});
+      song = new SongMetaData({'composer': 'c', 'artist': 'a', 'genre': 'g'});
       assert.equal(song.composer, 'c, a, g');
     });
     it('no properties', () => {
-      song = new Song({});
+      song = new SongMetaData({});
       assert.equal(song.composer, '');
     });
   });
 
   describe('get lyricist', () => {
     it('all properties', () => {
-      song = new Song({'lyricist': 'l'});
+      song = new SongMetaData({'lyricist': 'l'});
       assert.equal(song.lyricist, 'l');
     });
     it('no properties', () => {
-      song = new Song({});
+      song = new SongMetaData({});
       assert.equal(song.lyricist, '');
     });
   });
