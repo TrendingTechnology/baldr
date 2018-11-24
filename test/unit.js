50e22bde24df6670600b4083a000ec212cb93dfe
Josef Friedrich <josef@friedrich.rocks>
2018-11-24 16:58:50 +0100

    Convert to standardjs

--- test/unit.js
+++ test/unit.js
@@ -1,93 +1,89 @@
-const {bootstrapConfig, Library, SongMetaData} = require('../lib/without-dom.js');
-const assert = require('assert');
-const path = require('path');
+const { bootstrapConfig, Library, SongMetaData } = require('../lib/without-dom.js')
+const assert = require('assert')
+const path = require('path')
 
 describe('Function “bootstrapConfig”', () => {
   before(() => {
-    process.env.BALDR_SONGBOOK_PATH = '/lol';
-  });
+    process.env.BALDR_SONGBOOK_PATH = '/lol'
+  })
 
   it('BALDR_SONGBOOK_PATH', () => {
-    config = bootstrapConfig();
-    assert.equal(config.path, '/lol');
-  });
-});
+    let config = bootstrapConfig()
+    assert.strictEqual(config.path, '/lol')
+  })
+})
 
 describe('Class “Library”', () => {
-
   before(() => {
-    this.library = new Library(path.join(__dirname, 'songs', 'songs.json'));
-  });
+    this.library = new Library(path.join(__dirname, 'songs', 'songs.json'))
+  })
 
   describe('properties', () => {
     it('tree', () => {
-      assert.ok(this.library.tree);
-      assert.equal(
+      assert.ok(this.library.tree)
+      assert.strictEqual(
         this.library.tree.a['Auf-der-Mauer_auf-der-Lauer'].title,
         'Auf der Mauer, auf der Lauer'
-      );
-    });
+      )
+    })
     it('list', () => {
-      assert.ok(this.library.list);
-      assert.equal(
+      assert.ok(this.library.list)
+      assert.strictEqual(
         this.library.list['Auf-der-Mauer_auf-der-Lauer'].title,
         'Auf der Mauer, auf der Lauer'
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe('methods', () => {
     it('flattenTree_', () => {
-      let list = Library.flattenTree_(this.library.tree);
-    });
-  });
-
-});
+      Library.flattenTree_(this.library.tree)
+    })
+  })
+})
 
 describe('Class “SongMetaData”', () => {
-
   describe('get title', () => {
     it('title only', () => {
-      song = new SongMetaData({'title': 'lol'});
-      assert.equal(song.title, 'lol');
-    });
+      let song = new SongMetaData({ 'title': 'lol' })
+      assert.strictEqual(song.title, 'lol')
+    })
     it('title and year', () => {
-      song = new SongMetaData({'title': 'lol', 'year': 1984});
-      assert.equal(song.title, 'lol (1984)');
-    });
-  });
+      let song = new SongMetaData({ 'title': 'lol', 'year': 1984 })
+      assert.strictEqual(song.title, 'lol (1984)')
+    })
+  })
 
   describe('get subtitle', () => {
     it('all properties', () => {
-      song = new SongMetaData({'subtitle': 's', 'alias': 'a', 'country': 'c'});
-      assert.equal(song.subtitle, 's - a - c');
-    });
+      let song = new SongMetaData({ 'subtitle': 's', 'alias': 'a', 'country': 'c' })
+      assert.strictEqual(song.subtitle, 's - a - c')
+    })
     it('no properties', () => {
-      song = new SongMetaData({});
-      assert.equal(song.subtitle, '');
-    });
-  });
+      let song = new SongMetaData({})
+      assert.strictEqual(song.subtitle, '')
+    })
+  })
 
   describe('get composer', () => {
     it('all properties', () => {
-      song = new SongMetaData({'composer': 'c', 'artist': 'a', 'genre': 'g'});
-      assert.equal(song.composer, 'c, a, g');
-    });
+      let song = new SongMetaData({ 'composer': 'c', 'artist': 'a', 'genre': 'g' })
+      assert.strictEqual(song.composer, 'c, a, g')
+    })
     it('no properties', () => {
-      song = new SongMetaData({});
-      assert.equal(song.composer, '');
-    });
-  });
+      let song = new SongMetaData({})
+      assert.strictEqual(song.composer, '')
+    })
+  })
 
   describe('get lyricist', () => {
     it('all properties', () => {
-      song = new SongMetaData({'lyricist': 'l'});
-      assert.equal(song.lyricist, 'l');
-    });
+      let song = new SongMetaData({ 'lyricist': 'l' })
+      assert.strictEqual(song.lyricist, 'l')
+    })
     it('no properties', () => {
-      song = new SongMetaData({});
-      assert.equal(song.lyricist, '');
-    });
-  });
-
-});
+      let song = new SongMetaData({})
+      assert.strictEqual(song.lyricist, '')
+    })
+  })
+})
