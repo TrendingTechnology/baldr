8b0413b1e00efabda8e234398ef70c2ba74e1bb2
Josef Friedrich <josef@friedrich.rocks>
2018-11-24 13:33:18 +0100

    Merge check.js into index.js

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const { assert } = require('./lib/helper.js')
+const CheckChange = require('../check.js')
+const checkRewired = require('rewire')('../check.js')
 const fs = require('fs-extra')
 const indexRewired = require('rewire')('../index.js')
 const json = require('../json.js')
@@ -17,6 +19,57 @@ bootstrapConfig({
 })
 
 process.env.PATH = path.join(__dirname, 'bin:', process.env.PATH)
+
+describe('file “check.js”', () => {
+  it('object “Sqlite()”', () => {
+    let Sqlite = checkRewired.__get__('Sqlite')
+    let db = new Sqlite('test.db')
+
+    db.initialize()
+    assert.exists('test.db')
+
+    db.insert('lol', 'toll')
+    var row = db.select('lol')
+    assert.strictEqual(row.hash, 'toll')
+
+    try {
+      db.insert('lol', 'toll')
+    } catch (e) {
+      assert.strictEqual(e.name, 'SqliteError')
+    }
+
+    db.update('lol', 'troll')
+    assert.strictEqual(db.select('lol').hash, 'troll')
+
+    fs.unlinkSync('test.db')
+  })
+
+  it('function “hashSHA1()”', () => {
+    let hashSHA1 = checkRewired.__get__('hashSHA1')
+    assert.strictEqual(
+      hashSHA1(path.join('test', 'files', 'hash.txt')),
+      '7516f3c75e85c64b98241a12230d62a64e59bce3'
+    )
+  })
+
+  it('object “CheckChange()”', () => {
+    var check = new CheckChange()
+    check.init('test.db')
+    assert.strictEqual(check.db.dbFile, 'test.db')
+
+    fs.appendFileSync('tmp.txt', 'test')
+    assert.ok(check.do('tmp.txt'))
+
+    assert.ok(!check.do('tmp.txt'))
+    assert.ok(!check.do('tmp.txt'))
+
+    fs.appendFileSync('tmp.txt', 'test')
+    assert.ok(check.do('tmp.txt'))
+
+    fs.unlinkSync('tmp.txt')
+    fs.unlinkSync('test.db')
+  })
+})
 
 describe('Class “TeX”', () => {
   let TeX = indexRewired.__get__('TeX')
