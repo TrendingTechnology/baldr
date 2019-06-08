be2e3cffa911f4ddeb34da32e955691db56ce403
Josef Friedrich <josef@friedrich.rocks>
2019-06-08 16:13:39 +0200

    New package: @electron-apps-installer

---
+++ src/electron-apps-installer/index.js
@@ -0,0 +1,100 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const process = require('process')
+const util = require('util')
+
+const packager = require('electron-packager')
+
+let currentUser = os.userInfo()
+
+if (currentUser.username !== 'root') {
+  throw new Error('You must be root!')
+}
+
+let platform = os.platform()
+
+let destPrefix
+if (platform === 'darwin') {
+  destPrefix = path.join('/', 'Applications')
+} else {
+  destPrefix = path.join('/', 'opt', 'electron')
+}
+
+/**
+ *
+ * @param {string} packageName - The name of the node module on npmjs.com
+ * @param {string} destName - The name of the installed electron package on the
+ *   destination path.
+ */
+async function packageElectronApp (packageName, destName) {
+  console.log(util.format('Package node package “%s” into a electron app.', packageName))
+
+  let packagePath = path.dirname(require.resolve(packageName))
+  let packageJson = require(path.join(packagePath, 'package.json'))
+
+  // /opt/electron/songbook
+  let out = path.join(destPrefix, destName)
+  fs.mkdirSync(out, { 'recursive': true })
+
+  // @bldr-songbook-electron-app
+  let executableName = packageJson.name.replace('/', '-')
+
+  // @bldr-songbook-electron-app-linux-x64
+  let folderName = util.format('%s-%s-%s', executableName, platform, process.arch)
+  // let darwinPath = []
+  // if (process.platform === 'darwin') {
+  //   darwinPath = [electronName + '.app', 'Contents', 'MacOS']
+  // }
+  // this.appPath = path.join(this.distFolder, appName, ...darwinPath, electronName)
+
+  if (platform === 'linux') {
+    // Icons
+    let iconPath = path.join('/', 'usr', 'share', 'icons', 'baldr-electron-apps')
+    let iconSrc = path.join(packagePath, 'icon.iconset', 'icon_256x256.png')
+    let iconDest = path.join(iconPath, destName + '.png')
+    fs.mkdirSync(iconPath, { 'recursive': true })
+    fs.copyFileSync(iconSrc, iconDest)
+    console.log(util.format('Copy icon from %s to %s', iconSrc, iconDest))
+
+    // *.desktop file
+    let desktopFile = `[Desktop Entry]
+Name=${destName}
+Exec=${destPrefix}/${destName}/${folderName}/${executableName}
+Icon=${iconPath}/${destName}.png
+Terminal=false
+Type=Application`
+    let desktopPrefix = path.join('/', 'usr', 'share', 'applications')
+    let desktopPath = path.join(desktopPrefix, destName + '.desktop')
+    fs.appendFileSync(desktopPath, desktopFile)
+    console.log(util.format('Create *.desktop file at the location: %s', desktopPath))
+  }
+
+  console.log(util.format('Destination folder “%s”', out))
+  return packager({
+    // name: destName,
+    // executableName: 'entry-point',
+    dir: packagePath,
+    out: out,
+    prune: false,
+    // derefSymlinks: lerna symlinks the same dependencies.
+    // This symlinks are broken without the option derefSymlinks in the folder
+    // packages/electron-app/dist/@bldr-songbook-electron-app-linux-x64/resources/app/node_modules/@bldr
+    derefSymlinks: true,
+    overwrite: true,
+    arch: process.arch,
+    icon: path.join(packagePath, 'icon.icns'),
+    appVersion: packageJson.version,
+    asar: false // Maybe asar true is very slow.
+  })
+}
+
+async function packageElectronApps () {
+  await packageElectronApp('@bldr/songbook-electron-app', 'songbook')
+  await packageElectronApp('@bldr/camera-electron-app', 'camera')
+  await packageElectronApp('@bldr/electron-app', 'baldr')
+}
+
+packageElectronApps()
