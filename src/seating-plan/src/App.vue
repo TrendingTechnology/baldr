880b8b479199343d885333c0035224524bbe8e53
Josef Friedrich <josef@friedrich.rocks>
2019-06-17 16:46:04 +0200

    Add people class

--- src/seating-plan/src/App.vue
+++ src/seating-plan/src/App.vue
@@ -10,20 +10,52 @@ import Seat from './components/Seat.vue'
 import SeatingPlan from './components/SeatingPlan.vue'
 import PeopleList from './components/PeopleList.vue'
 
-
-let people = [
-  { firstName: 'Wolfgang Amadeus', lastName: 'Mozart' },
-  { firstName: 'Joseph', lastName: 'Haydn' },
-  { firstName: 'Ludwig van', lastName: 'Beethoven' },
-  { firstName: 'Johann Sebastian', lastName: 'Bach' },
-  { firstName: 'Georg Friedrich', lastName: 'Händel' }
+let peopleList = [
+  { firstName: 'Wolfgang Amadeus', lastName: 'Mozart', grade: 'Classic' },
+  { firstName: 'Joseph', lastName: 'Haydn', grade: 'Classic' },
+  { firstName: 'Ludwig van', lastName: 'Beethoven', grade: 'Classic' },
+  { firstName: 'Johann Sebastian', lastName: 'Bach', grade: 'Baroque' },
+  { firstName: 'Georg Friedrich', lastName: 'Händel', grade: 'Baroque' }
 ]
 
-let roomWidth = 100 // %
-let roomDepth = 100 // %
-let aisle = 0.10 * roomWidth
-let seatWidth = (roomWidth - aisle) / 8
-let seatDepth = (roomDepth - (3 * aisle)) / 4
+class Human {
+  constructor (firstName, lastName, grade) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.grade = grade
+  }
+
+  get sortString () {
+    return `${this.grade}: ${this.lastName}, ${this.firstName}`
+  }
+}
+
+class People {
+  constructor () {
+    this.list = {}
+  }
+
+  add (human) {
+    // grade
+    if (this.list.hasOwnProperty(human.grade)) {
+      this.list[human.grade]
+    } else {
+      this.list[human.grade] = {}
+    }
+
+    // lastName
+    if (this.list[human.grade].hasOwnProperty(human.lastName)) {
+      this.list[human.grade][human.lastName].push(human)
+    } else {
+      this.list[human.grade][human.lastName] = [human]
+    }
+  }
+}
+
+let people = new People()
+for (let humanFromList of peopleList) {
+  people.add(new Human(humanFromList.firstName, humanFromList.lastName, humanFromList.grade))
+}
 
 /**
  * 25 26 27 28    29 30 31 32
@@ -34,28 +66,42 @@ let seatDepth = (roomDepth - (3 * aisle)) / 4
  *
  * 1  2  3  4     5  6  7  8
  */
+function createSeats () {
+  let roomWidth = 100 // %
+  let roomDepth = 100 // %
+  let aisle = 0.10 * roomWidth
+  let seatWidth = (roomWidth - aisle) / 8
+  let seatDepth = (roomDepth - (3 * aisle)) / 4
 
-let seatBlocks = [
-  [[1, 2, 3, 4], [5, 6, 7, 8]],
-  [[9, 10, 11, 12], [13, 14, 15, 16]],
-  [[17, 18, 19, 20], [21, 22, 23, 24]],
-  [[25, 26, 27, 28], [29, 30, 31, 32]],
-]
+  let seatBlocks = [
+    [[1, 2, 3, 4], [5, 6, 7, 8]],
+    [[9, 10, 11, 12], [13, 14, 15, 16]],
+    [[17, 18, 19, 20], [21, 22, 23, 24]],
+    [[25, 26, 27, 28], [29, 30, 31, 32]],
+  ]
 
-let seats = []
-let seatY = 0
-let seatX = 0
-for (let row of seatBlocks) {
-  for (let block of row) {
-    for (let seatNo of block) {
-      seats.push({ no: seatNo, x: seatX, y: seatY })
-      seatX += seatWidth
+  let seats = []
+  let seatY = 0
+  let seatX = 0
+  for (let row of seatBlocks) {
+    for (let block of row) {
+      for (let seatNo of block) {
+        seats.push({ no: seatNo, x: seatX, y: seatY })
+        seatX += seatWidth
+      }
+      seatX += aisle
     }
-    seatX += aisle
+    seatY += seatDepth + aisle
+    seatX = 0
   }
-  seatY += seatDepth + aisle
-  seatX = 0
+  return {
+    seats: seats,
+    seatWidth: seatWidth,
+    seatDepth: seatDepth
+  }
 }
+
+let seats = createSeats()
 
 export default {
   name: 'app',
@@ -64,10 +110,10 @@ export default {
   },
   data: function () {
     return {
-      seats: seats,
-      seatDepth: seatDepth,
-      seatWidth: seatWidth,
-      people: people
+      seats: seats.seats,
+      seatDepth: seats.seatDepth,
+      seatWidth: seats.seatWidth,
+      people: peopleList
     }
   }
 }
