b9de8c191e51f35563e8265f1495bc908606e851
Josef Friedrich <josef@friedrich.rocks>
2019-06-22 07:28:01 +0200

    Add vue app to test default style

---
+++ themes/example-markup/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <LoremIpsum/>
+  </div>
+</template>
+
+<script>
+import LoremIpsum from './components/LoremIpsum.vue'
+import '@bldr/theme-default-css'
+
+export default {
+  name: 'app',
+  components: {
+    LoremIpsum
+  }
+}
+</script>
---
+++ themes/example-markup/src/components/LoremIpsum.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="lorem">
+
+    <h1>Lorem ipsum</h1>
+
+    <p>Paragraph: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+    diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet.</p>
+
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+
+    Person: <p class="person">Ludwig van Beethoven</p>
+    Piece: <p class="piece">Symphonie Nr. 5</p>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoremIpsum'
+}
+</script>
