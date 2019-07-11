86b049c00e144c3070e96b05c3c18511ffaed939
Josef Friedrich <josef@friedrich.rocks>
2019-07-11 09:51:14 +0200

    GradesList

--- src/seating-plan/src/components/GradesList.vue
+++ src/seating-plan/src/components/GradesList.vue
@@ -1,11 +1,16 @@
 <template>
-  <ul class="grades-list">
-    <li v-for="gradeName in gradeNames" :key="gradeName">
-      <router-link :to="'/grade/' + gradeName">
-        {{ gradeName }}
-      </router-link>
-    </li>
-  </ul>
+  <div class="grades-list">
+    <ul>
+      <li v-for="gradeName in gradeNames" :key="gradeName">
+        <router-link :to="'/grade/' + gradeName">
+          {{ gradeName }}
+        </router-link>
+      </li>
+    </ul>
+
+    <input v-model="gradeName" type="text" placeholder="Klasse">
+    <button @click="addGrde">hinzufügen</button>
+  </div>
 </template>
 
 <script>
@@ -13,6 +18,19 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'GradesList',
-  computed: mapGetters(['gradeNames'])
+  computed: mapGetters(['gradeNames']),
+  data: function () {
+    return {
+      gradeName: ''
+    }
+  },
+  methods: {
+    addGrade () {
+      if (this.gradeName) {
+        this.$store.dispatch('addGrade', { firstName: this.firstName, lastName: this.lastName, grade: this.currentGrade })
+        this.gradeName = ''
+      }
+    }
+  }
 }
 </script>
