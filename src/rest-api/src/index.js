2f365f5524d6a54b75bd807793403ca4c15b4909
Josef Friedrich <josef@friedrich.rocks>
2019-07-15 23:32:52 +0200

    rest api: write files

--- src/rest-api/src/index.js
+++ src/rest-api/src/index.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import fs from 'fs'
+// import path from 'path'
 
+const PORT = 5000
+// const STOREDIR = path.join('/', 'var', 'data')
 const app = express()
 
 app.use(express.json())
@@ -7,16 +11,25 @@ app.use(express.json())
 app.get('/api', (req, res) => {
   res.status(200).send({
     success: 'true',
-    message: 'todos retrieved successfully',
+    message: 'todos retrieved successfully'
   })
 })
 
 app.post('/api/store', (req, res) => {
-  console.log(req.body)
-  res.json({requestBody: req.body})
-})
+  const timeStampMsec = new Date().getTime()
+  const body = JSON.stringify(req.body)
+  fs.writeFile(`./seating-plan_${timeStampMsec}.json`, body, { encoding: 'utf-8' }, (err) => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
 
-const PORT = 5000
+  const responseMessage = {
+    timeStampMsec: timeStampMsec,
+    storedObject: req.body
+  }
+  res.json(responseMessage)
+  console.log(responseMessage)
+})
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
