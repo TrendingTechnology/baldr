3c0999109b199dbba0dde05f2c2269d8aecda9c6
Josef Friedrich <josef@friedrich.rocks>
2019-07-17 08:46:10 +0200

    Add commander support for the REST API

---
+++ src/rest-api/index.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+
+// Node packages.
+const fs = require('fs')
+
+// Third party packages.
+const { Command } = require('commander')
+const express = require('express')
+
+// Project packages.
+const package = require('./package.json')
+
+const PORT = 46328
+// const STOREDIR = path.join('/', 'var', 'data')
+const app = express()
+
+app.use(express.json())
+
+app.get('/version', (req, res) => {
+  res.status(200).send({
+    version: package.version
+  })
+})
+
+app.post('/store', (req, res) => {
+  const timeStampMsec = new Date().getTime()
+  const body = JSON.stringify(req.body)
+  fs.writeFile(`./seating-plan_${timeStampMsec}.json`, body, { encoding: 'utf-8' }, (err) => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+
+  const responseMessage = {
+    timeStampMsec: timeStampMsec,
+    storedObject: req.body
+  }
+  res.json(responseMessage)
+  console.log(responseMessage)
+})
+
+const main = function () {
+  // To get a clean commander. Otherwise we get options from mocha in the tests.
+  // https://github.com/tj/commander.js/issues/438#issuecomment-274285003
+  const commander = new Command()
+  const options = commander
+    .version(package.version)
+    .option('-p, --port <port>', 'Port to listen to.')
+    .parse(process.argv)
+
+  let port
+
+  if (options.port) {
+    port = options.port
+  } else if ({}.hasOwnProperty.call(process.env, 'BALDR_REST_API_PORT') &&
+             process.env.BALDR_REST_API_PORT) {
+    port = process.env.BALDR_REST_API_PORT
+  } else {
+    port = PORT
+  }
+  app.listen(port, () => {
+    console.log(`The BALDR REST API is running on port ${port}.`)
+  })
+}
+
+main()
