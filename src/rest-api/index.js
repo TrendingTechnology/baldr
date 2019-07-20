d6a04d7180d023477916b2fa6f0c81479d035a9b
Josef Friedrich <josef@friedrich.rocks>
2019-07-20 15:04:28 +0200

    Data store for rest api

--- src/rest-api/index.js
+++ src/rest-api/index.js
@@ -2,33 +2,40 @@
 
 // Node packages.
 const fs = require('fs')
+const path = require('path')
 
 // Third party packages.
 const { Command } = require('commander')
 const express = require('express')
 
 // Project packages.
-const package = require('./package.json')
+const packageJson = require('./package.json')
 
 const PORT = 46328
-// const STOREDIR = path.join('/', 'var', 'data')
+const STORE = process.env.BALDR_REST_API_STORE
+
 const app = express()
 
 app.use(express.json())
 
 app.get('/api/version', (req, res) => {
   res.status(200).send({
-    version: package.version
+    version: packageJson.version
   })
 })
 
 app.post('/api/store', (req, res) => {
   const timeStampMsec = new Date().getTime()
   const body = JSON.stringify(req.body)
-  fs.writeFile(`./seating-plan_${timeStampMsec}.json`, body, { encoding: 'utf-8' }, (err) => {
-    if (err) throw err
-    console.log('The file has been saved!')
-  })
+  fs.writeFile(
+    path.join(STORE, `seating-plan_${timeStampMsec}.json`),
+    body,
+    { encoding: 'utf-8' },
+    (err) => {
+      if (err) throw err
+      console.log('The file has been saved!')
+    }
+  )
 
   const responseMessage = {
     timeStampMsec: timeStampMsec,
@@ -43,7 +50,7 @@ const main = function () {
   // https://github.com/tj/commander.js/issues/438#issuecomment-274285003
   const commander = new Command()
   const options = commander
-    .version(package.version)
+    .version(packageJson.version)
     .option('-p, --port <port>', 'Port to listen to.')
     .parse(process.argv)
 
