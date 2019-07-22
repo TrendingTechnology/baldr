0ff953b8b008505e816fbf9b29900cf9ad25fa0b
Josef Friedrich <josef@friedrich.rocks>
2019-07-22 15:10:12 +0200

    Better date string

--- src/seating-plan/src/lib.js
+++ src/seating-plan/src/lib.js
@@ -1,6 +1,23 @@
 export function toLocaleDateTimeString (timeStampMsec) {
   const date = new Date(Number(timeStampMsec))
+  const dayNumber = date.getDay()
+  let dayString
+  if (dayNumber === 0) {
+    dayString = 'So'
+  } else if (dayNumber === 1) {
+    dayString = 'Mo'
+  } else if (dayNumber === 2) {
+    dayString = 'Di'
+  } else if (dayNumber === 3) {
+    dayString = 'Mi'
+  } else if (dayNumber === 4) {
+    dayString = 'Do'
+  } else if (dayNumber === 5) {
+    dayString = 'Fr'
+  } else if (dayNumber === 6) {
+    dayString = 'Sa'
+  }
   const dateString = date.toLocaleDateString()
   const timeString = date.toLocaleTimeString()
-  return `${dateString} ${timeString}`
+  return `${dayString} ${dateString} ${timeString}`
 }
