784a4175fc78a80cffb40595f0eda511d84bf44e
Josef Friedrich <josef@friedrich.rocks>
2019-08-04 08:23:53 +0200

    Generate boilerplates for vue app for the songbook

---
+++ masters/songbook/packages/vue-app/vue.config.js
@@ -0,0 +1,24 @@
+const os = require('os')
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+// https://forum.vuejs.org/t/vue-cli-does-not-work-with-symlinked-node-modules-using-lerna/61700
+// https://cli.vuejs.org/guide/troubleshooting.html#symbolic-links-in-node-modules
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.symlinks(false)
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        $HOME: os.homedir()
+      }
+    },
+    plugins: [
+      new CopyPlugin([
+        { from: '/home/jf/.local/share/baldr/projector', to: 'songs' }
+      ]),
+    ]
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/seating-plan/' : '/'
+}
