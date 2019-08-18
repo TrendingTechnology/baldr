53606ab7925fcd61284819c58bc1e8d1d2175fd9
Josef Friedrich <josef@friedrich.rocks>
2019-08-18 11:52:52 +0200

    Icons

---
+++ src/vue/apps/showroom/src/views/HomeView/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <section class="sub-projects">
+      <circle-icon href="https://baldr.friedrich.rocks/seating-plan" icon="seat-outline"/>
+      <circle-icon href="https://baldr.friedrich.rocks/songbook" icon="music" color="green"/>
+      <circle-icon icon="cctv" color="red"/>
+    </section>
+  </main>
+</template>
+
+<script>
+import CircleIcon from './CircleIcon'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CircleIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .sub-projects {
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
