451bb4b9416efa7ffaf241d5741ad229204543fa
Josef Friedrich <josef@friedrich.rocks>
2019-08-31 21:49:57 +0200

    Working on the media server

--- src/media-server/src/cli.js
+++ src/media-server/src/cli.js
@@ -1,158 +1,11 @@
 #! /usr/bin/env node
 
-// Node packages.
-const fs = require('fs')
-const path = require('path')
-
 // Third party packages.
 const commander = require('commander')
-const glob = require('glob')
-const Sqlite3 = require('better-sqlite3')
-const yaml = require('js-yaml')
 
-let basePath
+const { setup, update, list } = require('./index.js')
+
 let subcommand
-
-/**
- *
- */
-class MetaData {
-  constructor (filePath) {
-    /**
-     * Absolute path ot the file.
-     * @type {string}
-     */
-    this.path = filePath.replace(basePath, '').replace(/^\//, '')
-
-    /**
-     * The basename (filename) of the file.
-     * @type {string}
-     */
-    this.filename = path.basename(filePath)
-
-    /**
-     * The extension of the file.
-     * @type {string}
-     */
-    this.extension = path.extname(filePath).replace('.', '')
-
-    const data = this.readInfoYaml_()
-
-    /**
-     * @type {string}
-     */
-    this.id = ''
-    if ('id' in data) {
-      this.id = data.id
-    }
-
-    /**
-     * @type {object}
-     */
-    this.data = JSON.stringify(data)
-  }
-
-  /**
-   * Parse the info file of a media file.
-   *
-   * Each media file can have a info file that stores additional
-   * metadata informations.
-   *
-   * File path:
-   * `/home/baldr/beethoven.jpg`
-   *
-   * Info file in the YAML file format:
-   * `/home/baldr/beethoven.jpg.yml`
-   */
-  readInfoYaml_ () {
-    const infoFile = this.path + '.yml'
-    if (fs.existsSync(infoFile)) {
-      return yaml.safeLoad(fs.readFileSync(infoFile, 'utf8'))
-    }
-    return {}
-  }
-}
-
-/**
- * Sqlite database wrapper.
- */
-class Sqlite {
-  /**
-   * @param {string} dbFile - The path of the Sqlite database.
-   */
-  constructor (dbFile) {
-    /**
-     * The path of the Sqlite database.
-     *
-     * @type {string}
-     */
-    this.dbFile = dbFile
-
-    /**
-     * A instance of the class “Sqlite3”.
-     */
-    this.db = new Sqlite3(this.dbFile)
-    this.prepare_(
-      `CREATE TABLE IF NOT EXISTS files (
-        path TEXT UNIQUE,
-        filename TEXT,
-        extension TEXT,
-        id TEXT UNIQUE,
-        data TEXT
-      )`
-    )
-
-    this.prepare_('CREATE INDEX IF NOT EXISTS id ON files(id)')
-    this.prepare_('CREATE INDEX IF NOT EXISTS filename ON files(filename)')
-  }
-
-  prepare_ (sql, data) {
-    if (data) {
-      this.db.prepare(sql).run(data)
-    } else {
-      this.db.prepare(sql).run()
-    }
-  }
-
-  update ({ path, filename, extension, id, data }) {
-    this.prepare_(
-      'INSERT OR IGNORE INTO files (path) VALUES ($path)',
-      { path: path }
-    )
-
-    this.prepare_(
-      `UPDATE files
-        SET
-          path = $path,
-          filename = $filename,
-          extension = $extension,
-          id = $id,
-          data = $data
-        WHERE path = $path
-      `,
-      {
-        path: path,
-        filename: filename,
-        extension: extension,
-        id: id,
-        data: data
-      }
-    )
-  }
-
-  list () {
-    return this.db
-      .prepare('SELECT * FROM files')
-      .all()
-  }
-
-  /**
-   * Delete all rows from the table “hashes”.
-   */
-  flush () {
-    this.db.prepare('DELETE FROM hashes').run()
-  }
-}
 
 commander
   .version(require('../package.json').version)
@@ -170,24 +23,12 @@ commander
 
 commander.parse(process.argv)
 
-let dbFile
 if (commander.basePath) {
-  basePath = commander.basePath
-  dbFile = path.join(basePath, 'files.db')
+  setup(commander.basePath)
 }
 
-const sqlite = new Sqlite(dbFile)
-
 if (subcommand === 'update') {
-  const files = glob.sync(path.join(basePath, '**/*'), { ignore: ['**/*.db'] })
-  for (const file of files) {
-    if (!fs.lstatSync(file).isDirectory()) {
-      const metaData = new MetaData(file)
-      console.log(metaData)
-      sqlite.update(metaData)
-    }
-  }
+  update()
 } else if (subcommand === 'list') {
-  const result = sqlite.list()
-  console.log(result)
+  list()
 }
---
+++ src/media-server/src/index.js
@@ -0,0 +1,198 @@
+#! /usr/bin/env node
+
+// Node packages.
+const fs = require('fs')
+const path = require('path')
+
+// Third party packages.
+const glob = require('glob')
+const Sqlite3 = require('better-sqlite3')
+const yaml = require('js-yaml')
+
+// Project packages.
+const { utils } = require('@bldr/core')
+
+let basePath
+let sqlite
+
+/**
+ *
+ */
+class MetaData {
+  constructor (filePath) {
+    /**
+     * Absolute path ot the file.
+     * @type {string}
+     */
+    this.path = filePath.replace(basePath, '').replace(/^\//, '')
+
+    /**
+     * The basename (filename) of the file.
+     * @type {string}
+     */
+    this.filename = path.basename(filePath)
+
+    /**
+     * The extension of the file.
+     * @type {string}
+     */
+    this.extension = path.extname(filePath).replace('.', '')
+
+    const data = this.readInfoYaml_()
+
+    /**
+     * @type {string}
+     */
+    this.id = ''
+    if ('id' in data) {
+      this.id = data.id
+    }
+
+    /**
+     * @type {object}
+     */
+    this.data = JSON.stringify(data)
+  }
+
+  /**
+   * Parse the info file of a media file.
+   *
+   * Each media file can have a info file that stores additional
+   * metadata informations.
+   *
+   * File path:
+   * `/home/baldr/beethoven.jpg`
+   *
+   * Info file in the YAML file format:
+   * `/home/baldr/beethoven.jpg.yml`
+   */
+  readInfoYaml_ () {
+    const infoFile = this.path + '.yml'
+    if (fs.existsSync(infoFile)) {
+      return yaml.safeLoad(fs.readFileSync(infoFile, 'utf8'))
+    }
+    return {}
+  }
+}
+
+/**
+ * Sqlite database wrapper.
+ */
+class Sqlite {
+  /**
+   * @param {string} dbFile - The path of the Sqlite database.
+   */
+  constructor (dbFile) {
+    /**
+     * The path of the Sqlite database.
+     *
+     * @type {string}
+     */
+    this.dbFile = dbFile
+
+    /**
+     * A instance of the class “Sqlite3”.
+     */
+    this.db = new Sqlite3(this.dbFile)
+    this.prepare_(
+      `CREATE TABLE IF NOT EXISTS files (
+        path TEXT UNIQUE,
+        filename TEXT,
+        extension TEXT,
+        id TEXT UNIQUE,
+        data TEXT
+      )`
+    )
+
+    this.prepare_('CREATE INDEX IF NOT EXISTS id ON files(id)')
+    this.prepare_('CREATE INDEX IF NOT EXISTS filename ON files(filename)')
+  }
+
+  prepare_ (sql, data) {
+    if (data) {
+      this.db.prepare(sql).run(data)
+    } else {
+      this.db.prepare(sql).run()
+    }
+  }
+
+  update ({ path, filename, extension, id, data }) {
+    this.prepare_(
+      'INSERT OR IGNORE INTO files (path) VALUES ($path)',
+      { path: path }
+    )
+
+    this.prepare_(
+      `UPDATE files
+        SET
+          path = $path,
+          filename = $filename,
+          extension = $extension,
+          id = $id,
+          data = $data
+        WHERE path = $path
+      `,
+      {
+        path: path,
+        filename: filename,
+        extension: extension,
+        id: id,
+        data: data
+      }
+    )
+  }
+
+  list () {
+    return this.db
+      .prepare('SELECT * FROM files')
+      .all()
+  }
+
+  /**
+   * Delete all rows from the table “hashes”.
+   */
+  flush () {
+    this.db.prepare('DELETE FROM hashes').run()
+  }
+}
+
+function setup (customBasePath) {
+  if (!customBasePath) {
+    const config = utils.bootstrapConfig()
+    basePath = config.mediaServer.path
+  } else {
+    basePath = customBasePath
+  }
+  const dbFile = path.join(basePath, 'files.db')
+  sqlite = new Sqlite(dbFile)
+}
+
+function update () {
+  const files = glob.sync(path.join(basePath, '**/*'), { ignore: ['**/*.db'] })
+  for (const file of files) {
+    if (!fs.lstatSync(file).isDirectory()) {
+      const metaData = new MetaData(file)
+      console.log(metaData)
+      sqlite.update(metaData)
+    }
+  }
+}
+
+function list () {
+  const result = sqlite.list()
+  console.log(result)
+}
+
+function queryByID (id) {
+  console.log(id)
+}
+
+function queryByFilename (filename) {
+  console.log(filename)
+}
+
+exports.setup = setup
+exports.update = update
+exports.list = list
+exports.queryByFilename = queryByFilename
+exports.queryByID = queryByID
