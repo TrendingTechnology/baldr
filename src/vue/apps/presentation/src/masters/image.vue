2d10b961145343d7f10f7c52b9cfbce1e0b40860
Josef Friedrich <josef@friedrich.rocks>
2019-09-04 08:45:42 +0200

    Use media server to resolve src

--- src/vue/apps/presentation/src/masters/image.vue
+++ src/vue/apps/presentation/src/masters/image.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="image-master">
-    <img :src="src"/>
+    <img :src="srcResolved"/>
   </div>
-
 </template>
 
 <script>
 const examples = [
   {
-    title: 'URL: https://',
+    title: 'URL: https:',
     data: {
       src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Johannes_Brahms_LCCN2016872659.jpg/1280px-Johannes_Brahms_LCCN2016872659.jpg'
+    }
+  },
+  {
+    title: 'URL: id:',
+    data: {
+      src: 'id:Haydn_Joseph'
+    }
+  },
+  {
+    title: 'URL: filename:',
+    data: {
+      src: 'filename:Beethoven_Ludwig-van.jpg'
     }
   }
 ]
@@ -28,6 +39,14 @@ export default {
       type: String,
       required: true
     }
+  },
+  computed: {
+    srcResolved () {
+      return this.$store.getters.media(this.src)
+    }
+  },
+  created () {
+    this.$store.dispatch('resolveMedia', this.src)
   }
 }
 </script>
--- src/vue/apps/presentation/src/store.js
+++ src/vue/apps/presentation/src/store.js
@@ -1,16 +1,35 @@
+/* globals config */
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { parseContentFile } from './content-file.js'
+import axios from 'axios'
+
+console.log(`Rest-API: Using username '${config.restApi.username}'`)
+console.log(`Rest-API: Using password '${config.restApi.password}'`)
+
+const axiosInstance = axios.create({
+  baseURL: 'https://baldr.friedrich.rocks/api/',
+  timeout: 3000,
+  auth: {
+    username: config.restApi.username,
+    password: config.restApi.password
+  }
+})
 
 Vue.use(Vuex)
 
 const state = {
+  media: {},
   mediaDevices: [],
-  slides: {},
-  slideNoCurrent: null
+  slideNoCurrent: null,
+  slides: {}
 }
 
 const getters = {
+  media: (state) => (URL) => {
+    return state.media[URL]
+  },
   mediaDevices: state => {
     return state.mediaDevices
   },
@@ -74,10 +93,44 @@ const actions = {
     } else {
       commit('setSlideNoCurrent', no - 1)
     }
+  },
+  resolveMedia ({ commit }, URL) {
+    const segments = URL.split(':')
+    const schemeName = segments[0]
+    const urlParameter = segments[1]
+    const conf = config.mediaServer
+    if (schemeName === 'http' || schemeName === 'https') {
+      commit('resolveMedia', { URL: URL, resolvedMedia: URL })
+    } else if (schemeName === 'id') {
+      axiosInstance
+        .post('/media-server/query-by-id', { id: urlParameter })
+        .then((response) => {
+          const data = response.data
+          commit('resolveMedia', {
+            URL: URL,
+            resolvedMedia: `http://${conf.domainRemote}:${conf.port}/${data.path}`
+          })
+        })
+        .catch(() => true)
+    } else if (schemeName === 'filename') {
+      axiosInstance
+        .post('/media-server/query-by-filename', { filename: urlParameter })
+        .then((response) => {
+          const data = response.data
+          commit('resolveMedia', {
+            URL: URL,
+            resolvedMedia: `http://${conf.domainRemote}:${conf.port}/${data.path}`
+          })
+        })
+        .catch(() => true)
+    }
   }
 }
 
 const mutations = {
+  resolveMedia (state, { URL, resolvedMedia }) {
+    Vue.set(state.media, URL, resolvedMedia)
+  },
   setMediaDevices (state, mediaDevices) {
     state.mediaDevices = mediaDevices
   },
