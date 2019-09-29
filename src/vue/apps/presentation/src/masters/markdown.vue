e329e0afc989c89d3693a3f02aaaed5ff84ca99f
Josef Friedrich <josef@friedrich.rocks>
2019-09-29 08:54:00 +0200

    split html

--- src/vue/apps/presentation/src/masters/markdown.vue
+++ src/vue/apps/presentation/src/masters/markdown.vue
@@ -10,6 +10,74 @@ const example = `
 ---
 slides:
 
+- title: Long text
+  markdown: |
+    # Heading 1
+
+    Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
+    tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea
+    commodi consequat. Quis aute iure reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+    cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
+    id est laborum.
+
+    # Heading 2
+
+    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+    molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero
+    eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
+    zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
+    dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+    euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+
+    # Heading 3
+
+    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+    suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem
+    vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+    consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
+    accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+    delenit augue duis dolore te feugait nulla facilisi.
+
+    Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet
+    doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit
+    amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+    tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+    minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis
+    nisl ut aliquip ex ea commodo consequat.
+
+    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+    molestie consequat, vel illum dolore eu feugiat nulla facilisis.
+
+    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At
+    accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy
+    sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd
+    magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem
+    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+    elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+    aliquyam erat.
+
+    Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+    sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+    sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet.
+
 - title: Heading
   markdown: |
     # heading 1
@@ -41,6 +109,25 @@ slides:
     * three
 `
 
+function splitHtmlintoChunks (htmlString) {
+  const domParser = new DOMParser()
+  const dom = domParser.parseFromString(htmlString, 'text/html')
+
+  const threshold = 500
+  let buffer = ''
+  const chunks = []
+
+  for (const children of dom.body.children) {
+    buffer += children.outerHTML
+    if (buffer.length > threshold) {
+      chunks.push(buffer)
+      buffer = ''
+    }
+  }
+
+  return chunks
+}
+
 export const master = {
   styleConfig: {
     centerVertically: true,
@@ -65,7 +152,8 @@ export default {
   },
   computed: {
     markupConverted () {
-      return marked(this.markup)
+      const htmlString = marked(this.markup)
+      return htmlString
     }
   }
 }
