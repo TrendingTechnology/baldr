c3aff676f0938c9b21c4b8553809eb0df4aa0a95
Josef Friedrich <josef@friedrich.rocks>
2019-10-14 22:23:20 +0200

    Improve editor

--- src/vue/apps/presentation/src/masters/editor.vue
+++ src/vue/apps/presentation/src/masters/editor.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="editor-master mousetrap" spellcheck="false" v-html="markup">
+  <div
+    class="editor-master"
+    spellcheck="false"
+    v-html="markup"
+    :style="{ fontSize: fontSize + 'vw' }">
   </div>
 </template>
 
@@ -46,6 +50,45 @@ slides:
         </tr>
       </tbody>
     </table>
+
+- title: 'HTML: <table*> replace with contenteditable'
+  editor: |
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <td>Thema 1 (Spanier)</td>
+          <td>Thema 2 (Niederländer)</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Dynamik</th>
+          <td>*</td>
+          <td>*</td>
+        </tr>
+        <tr>
+          <th>Rhythmik</th>
+          <td>*</td>
+          <td>*</td>
+        </tr>
+        <tr>
+          <th>Satztechnik</th>
+          <td>*</td>
+          <td>*</td>
+        </tr>
+        <tr>
+          <th>Artikulation</th>
+          <td>*</td>
+          <td>*</td>
+        </tr>
+        <tr>
+          <th>Tonlage</th>
+          <td>*</td>
+          <td>*</td>
+        </tr>
+      </tbody>
+    </table>
 `
 
 export const master = {
@@ -56,17 +99,19 @@ export const master = {
   },
   example,
   normalizeProps (props) {
+    let propsNormalized = {}
     if (typeof props === 'boolean') {
       // Somehow two editor slides get the same edited content.
       editorId += 1
-      return {
-        markup: `<p contenteditable class="editor-${editorId}">x</p>`
-      }
+      propsNormalized.markup = `<p class="editor-${editorId}" contenteditable>x</p>`
+
     } else if (typeof props === 'string') {
-      return {
-        markup: props
-      }
+      propsNormalized.markup = props
+    } else {
+      propsNormalized = props
     }
+    propsNormalized.markup = propsNormalized.markup.replace(/>\w*\*\w*</g, ' contenteditable>.<')
+    return propsNormalized
   },
   // Called when leaving a slide.
   leaveSlide ({ oldSlide, oldProps, newSlide, newProps }) {
@@ -81,6 +126,11 @@ export default {
       type: String
     }
   },
+  data () {
+    return {
+      fontSize: 2
+    }
+  },
   methods: {
     bold () {
       const selection = window.getSelection()
@@ -89,14 +139,26 @@ export default {
         const elStrong = document.createElement('strong')
         range.surroundContents(elStrong)
       }
+    },
+    increaseFontSize () {
+      this.fontSize += 0.25
+    },
+    decreaseFontSize () {
+      this.fontSize -= 0.25
     }
   },
   created () {
     // We can not use mousetrap because mousetrap is disable in contenteditable areas.
     document.addEventListener('keydown', (event) => {
-      if (event.ctrlKey && event.code === 'KeyB') {
+      if (event.ctrlKey && event.key === 'b') {
         event.preventDefault()
         this.bold()
+      } else if (event.ctrlKey && event.key === '+') {
+        event.preventDefault()
+        this.increaseFontSize()
+      } else if (event.ctrlKey && event.key === '-') {
+        event.preventDefault()
+        this.decreaseFontSize()
       }
     })
   }
