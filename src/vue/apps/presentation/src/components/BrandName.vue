36b6f69d58cf6f0befbfacea8cc97d7d7527d37b
Josef Friedrich <josef@friedrich.rocks>
2019-10-17 05:50:11 +0200

    Do not show brand name on home

---
+++ src/vue/apps/presentation/src/components/BrandName.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="brand">
+    <div>
+      <material-icon
+        size="20vw"
+        outline="circle"
+        name="presentation"
+        color="orange"
+      />
+    </div>
+    <div class="brand-name">
+      <div>
+        Baldr
+      </div>
+      <div>
+        Presentation
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BrandName'
+}
+</script>
+
+<style lang="scss" scoped>
+  .brand {
+    display: flex;
+    padding: 3vw 0;
+    text-align: center;
+
+    .brand-name {
+      color: $orange;
+      font-family: $font-family-sans-small-caps;
+      font-size: 8vw;
+      text-align: left;
+      font-weight: bold;
+      padding-left: 3vw;
+      padding-top: 6vw;
+    }
+  }
+</style>
--- src/vue/apps/presentation/src/views/Home.vue
+++ src/vue/apps/presentation/src/views/Home.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="home">
     <open-interface/>
-    <section class="brand">
-      <div>
-        <material-icon
-          size="20vw"
-          outline="circle"
-          name="presentation"
-          color="orange"
-        />
-      </div>
-      <div class="brand-name">
-        <div>
-          Baldr
-        </div>
-        <div>
-          Presentation
-        </div>
-      </div>
-    </section>
   </div>
 </template>
 
@@ -38,21 +20,5 @@ export default {
 <style lang="scss" scoped>
   .home {
     font-size: 2vw;
-
-    .brand {
-      display: flex;
-      padding: 3vw 0;
-      text-align: center;
-
-      .brand-name {
-        color: $orange;
-        font-family: $font-family-sans-small-caps;
-        font-size: 8vw;
-        text-align: left;
-        font-weight: bold;
-        padding-left: 3vw;
-        padding-top: 6vw;
-      }
-    }
   }
 </style>
