1b922d9753522d325e1cbcd9bde720ccb22f1cfc
Josef Friedrich <josef@friedrich.rocks>
2019-10-18 16:38:06 +0200

    Clean gh-pages

--- generate-docs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-mkdir -p /var/data/baldr/src
-
-git clone git@github.com:Josef-Friedrich/baldr.git /var/data/baldr/src
-
-jsdoc --configure ./jsdoc-config.json --destination . /var/data/baldr/src
