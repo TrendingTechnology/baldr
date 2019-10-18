c8a874088863f7f158e2d4da9152ffad28cc73a3
Josef Friedrich <josef@friedrich.rocks>
2019-10-18 11:16:28 +0200

    Extend scrpt

---
+++ README.md
@@ -0,0 +1,5 @@
+Generate the documentation for the baldr project using jsdoc.
+
+```sh
+./generate-docs.sh
+```
--- generate-docs.sh
+++ generate-docs.sh
@@ -1,7 +1,25 @@
 #! /bin/sh
 
-mkdir -p /var/data/baldr/src
+CWD=$(pwd)
 
-git clone git@github.com:Josef-Friedrich/baldr.git /var/data/baldr/src
+DIR_PARENT=/var/data/baldr
+DIR_SRC=$DIR_PARENT/src
+DIR_GH_PAGES=$DIR_PARENT/gh-pages
 
-jsdoc --configure ./jsdoc-config.json --destination . /var/data/baldr/src
+mkdir -p $DIR_SRC
+mkdir -p $DIR_GH_PAGES
+
+git clone git@github.com:Josef-Friedrich/baldr.git -b master $DIR_SRC
+cd $DIR_SRC
+git pull
+
+git clone git@github.com:Josef-Friedrich/baldr.git -b gh-pages $DIR_GH_PAGES
+cd $DIR_GH_PAGES
+git pull
+rm -rf *
+
+cd $CWD
+
+jsdoc --configure ./jsdoc-config.json --destination $DIR_GH_PAGES $DIR_SRC
+
+xdg-open $DIR_GH_PAGES/index.html
