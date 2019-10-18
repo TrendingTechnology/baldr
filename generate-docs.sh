12f77cd950cdb45d2514a278dd40fef439823407
Josef Friedrich <josef@friedrich.rocks>
2019-10-18 11:28:50 +0200

    Add vue js plugin

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,22 +3,32 @@
 CWD=$(pwd)
 
 DIR_PARENT=/var/data/baldr
+DIR_JSDOC=$DIR_PARENT/jsdoc
 DIR_SRC=$DIR_PARENT/src
 DIR_GH_PAGES=$DIR_PARENT/gh-pages
 
-mkdir -p $DIR_SRC
-mkdir -p $DIR_GH_PAGES
+# jsdoc
+mkdir -p $DIR_JSDOC
+git clone git@github.com:Josef-Friedrich/baldr.git -b jsdoc $DIR_JSDOC
 
+# SRC
+mkdir -p $DIR_SRC
 git clone git@github.com:Josef-Friedrich/baldr.git -b master $DIR_SRC
 cd $DIR_SRC
 git pull
 
+# GH_PAGES
+mkdir -p $DIR_GH_PAGES
 git clone git@github.com:Josef-Friedrich/baldr.git -b gh-pages $DIR_GH_PAGES
 cd $DIR_GH_PAGES
 git pull
 rm -rf *
 
 cd $CWD
+
+npm install -g jsdoc
+npm install -g jsdoc-vuejs
+npm install -g vue-template-compiler
 
 jsdoc --configure ./jsdoc-config.json --destination $DIR_GH_PAGES $DIR_SRC
 
