c5aea20dc07c8b554a06feac0e52c891a946db9a
Josef Friedrich <josef@friedrich.rocks>
2019-10-22 17:06:53 +0200

    Generate docs

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,29 +1,6 @@
 #! /bin/bash
 
-PACKAGES=(
-  'masters/songbook/src/base:songbook-base'
-  'masters/songbook/src/cli:songbook-cli'
-  'masters/songbook/src/core:songbook-core'
-  'masters/songbook/src/intermediate-files:songbook-intermediate-files'
-  'src/api-media-server:api-media-server'
-  'src/api-seating-plan:api-seating-plan'
-  'src/api:api'
-  'src/core-browser:core-browser'
-  'src/core:core'
-  'src/http-request:http-request'
-  'src/vue/apps/presentation:presentation'
-  'src/vue/apps/seating-plan:seating-plan'
-  'src/vue/apps/showroom:showroom'
-  'src/vue/apps/songbook:songbook-vue-app'
-  'src/vue/components/collection:vue-components-collection'
-  'src/vue/components/dynamic-select:vue-component-dynamic-select'
-  'src/vue/components/material-icon:vue-component-material-icon'
-  'src/vue/components/media:vue-media'
-  'src/vue/components/modal-dialog:vue-component-modal-dialog'
-  'src/vue/components/shortcuts:vue-shortcuts'
-  'themes/default:theme-default'
-  'themes/handwriting:theme-handwriting'
-)
+
 
 CWD="$(pwd)"
 
@@ -61,21 +38,58 @@ rm -rf *
 
 cd "$CWD"
 
-npm install -g jsdoc
-npm install -g jsdoc-vuejs
-npm install -g vue-template-compiler
-#npm install -g documentation
+function _install_node_dependencies() {
+	npm install -g jsdoc
+	npm install -g jsdoc-vuejs
+	npm install -g vue-template-compiler
+	#npm install -g documentation
+}
 
-cat index_header.html > "$DIR_GH_PAGES/index.html"
+function _separate_packages() {
+	PACKAGES=(
+		'masters/songbook/src/base:songbook-base'
+		'masters/songbook/src/cli:songbook-cli'
+		'masters/songbook/src/core:songbook-core'
+		'masters/songbook/src/intermediate-files:songbook-intermediate-files'
+		'src/api-media-server:api-media-server'
+		'src/api-seating-plan:api-seating-plan'
+		'src/api:api'
+		'src/core-browser:core-browser'
+		'src/core:core'
+		'src/http-request:http-request'
+		'src/vue/apps/presentation:presentation'
+		'src/vue/apps/seating-plan:seating-plan'
+		'src/vue/apps/showroom:showroom'
+		'src/vue/apps/songbook:songbook-vue-app'
+		'src/vue/components/collection:vue-components-collection'
+		'src/vue/components/dynamic-select:vue-component-dynamic-select'
+		'src/vue/components/material-icon:vue-component-material-icon'
+		'src/vue/components/media:vue-media'
+		'src/vue/components/modal-dialog:vue-component-modal-dialog'
+		'src/vue/components/shortcuts:vue-shortcuts'
+		'themes/default:theme-default'
+		'themes/handwriting:theme-handwriting'
+	)
+	cat index_header.html > "$DIR_GH_PAGES/index.html"
 
-for i in "${PACKAGES[@]}"; do
-	REL_PATH=${i%%:*}
-	PACKAGE_NAME=${i#*:}
-	jsdoc --configure ./jsdoc-config.json --destination "$DIR_GH_PAGES/$PACKAGE_NAME" "$DIR_SRC/$REL_PATH"
-	#documentation build "$DIR_SRC/$REL_PATH/**" --format html --output "$DIR_GH_PAGES/$PACKAGE_NAME"
-  echo "<li><a href=\"$PACKAGE_NAME/index.html\">@bldr/$PACKAGE_NAME</a></li>" >> "$DIR_GH_PAGES/index.html"
-done
+	for i in "${PACKAGES[@]}"; do
+		REL_PATH=${i%%:*}
+		PACKAGE_NAME=${i#*:}
+		jsdoc --configure ./jsdoc-config.json --destination "$DIR_GH_PAGES/$PACKAGE_NAME" "$DIR_SRC/$REL_PATH"
+		#documentation build "$DIR_SRC/$REL_PATH/**" --format html --output "$DIR_GH_PAGES/$PACKAGE_NAME"
+		echo "<li><a href=\"$PACKAGE_NAME/index.html\">@bldr/$PACKAGE_NAME</a></li>" >> "$DIR_GH_PAGES/index.html"
+	done
 
-cat index_footer.html >> "$DIR_GH_PAGES/index.html"
+	cat index_footer.html >> "$DIR_GH_PAGES/index.html"
+}
+
+function _one_big_package() {
+	jsdoc \
+		--configure ./jsdoc-config.json \
+		--destination "$DIR_GH_PAGES" \
+		"$DIR_SRC"
+}
+
+_one_big_package
 
 xdg-open "$DIR_GH_PAGES/index.html" &
