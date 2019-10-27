f1229e56f09b362c2530b6c2b0892c9922071a68
Josef Friedrich <josef@friedrich.rocks>
2019-10-27 18:07:35 +0100

    New class: Master()

--- src/vue-app-presentation/src/masters.js
+++ src/vue-app-presentation/src/masters.js
@@ -5,6 +5,79 @@
 import Vue from 'vue'
 import store from '@/store.js'
 
+/**
+ * Each master slide is a instance of this class. This class has many dummy
+ * methods. They are there for documentation reasons. On the other side they
+ * are useful as default methods. You have not to check if a master slide
+ * implements a specific hook.
+ */
+class Master {
+  constructor (name) {
+    /**
+     * It is the same as the basename of the Vue component, for example
+     * `audio.vue`. The name is `audio`.
+     * @type {string}
+     */
+    this.name = name
+  }
+
+  /**
+   *
+   * @param {*} config
+   */
+  importMembers (config) {
+    for (const member in config) {
+      this[member] = config[member]
+    }
+  }
+
+  /**
+   * result must fit to props
+   * @param {object} props
+   */
+  normalizeProps (props) {}
+
+  /**
+   *
+   * @param {*} props
+   */
+  stepCount (props) {}
+
+  /**
+   * An array of media URIs to resolve (like [id:beethoven, filename:mozart.mp3])
+   * @param {*} props
+   */
+  resolveMediaUris (props) {}
+
+  /**
+   *
+   * @param {*} props
+   */
+  plainTextFromProps (props) {}
+
+  /**
+   * Called when entering a slide.
+   * @param {*} param0
+   */
+  enterSlide ({ oldSlide, oldProps, newSlide, newProps }) {}
+
+  /**
+   * Called when leaving a slide.
+   * @param {*} param0
+   */
+  leaveSlide ({ oldSlide, oldProps, newSlide, newProps }) {}
+
+  /**
+   * Called when entering a step.
+   */
+  enterStep ({ oldStepNo, newStepNo }) {}
+
+  /**
+   * Called when leaving a step.
+   */
+  leaveStep ({ oldStepNo, newStepNo }) {}
+}
+
 // https://github.com/chrisvfritz/vue-enterprise-boilerplate/blob/master/src/components/_globals.js
 // https://webpack.js.org/guides/dependency-management/#require-context
 const requireComponent = require.context(
@@ -26,8 +99,9 @@ requireComponent.keys().forEach((fileName) => {
   const masterName = fileName.replace('./', '').replace('.vue', '')
   const componentConfig = requireComponent(fileName)
   const masterConfig = componentConfig.master
-  masters[masterName] = masterConfig
-  masters[masterName].name = masterName
+  const master = new Master(masterName)
+  master.importMembers(masterConfig)
+  masters[masterName] = master
   masters[masterName].vue = componentConfig.default
   // Remove the empty line at the beginning of the backtick string example.
   if ('example' in masters[masterName]) {
