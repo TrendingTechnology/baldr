fd0615ae7eb5e0b4577bcaaa2730121f1827e32e
Josef Friedrich <josef@friedrich.rocks>
2019-11-02 13:36:01 +0100

    Upgrade

---
+++ src/vue-plugin-dynamic-select/webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+}
