7722579a918a8255caf48a1a8c9cf6177ace18f6
Josef Friedrich <josef@friedrich.rocks>
2019-11-10 08:23:45 +0100

    New cli command: mirror

--- src/api-media-server/src/cli.js
+++ src/api-media-server/src/cli.js
@@ -602,11 +602,71 @@ function openBasePath () {
   process.unref()
 }
 
+/**
+ * Create and open a relative path in different base paths.
+ */
+function mirrorRelPath () {
+  const basePaths = [
+    '/var/data/baldr/media/',
+    '/home/jf/schule/',
+    '/mnt/nnas/school/archive/'
+  ]
+
+  const currentBasePaths = []
+  for (const basePath of basePaths) {
+    if (fs.existsSync(basePath)) {
+      currentBasePaths.push(basePath)
+    }
+  }
+
+  console.log(`This base paths exist or are accessible: ${chalk.yellow(currentBasePaths)}`)
+
+  const cwd = process.cwd()
+
+  const regex = /^[a-zA-Z0-9-_/]+$/g
+  if (!regex.test(cwd)) {
+    console.log(`The current working directory “${chalk.red(cwd)}” contains illegal characters.`)
+    return
+  }
+
+  function getRelPath () {
+    for (const basePath of currentBasePaths) {
+      if (cwd.indexOf(basePath) === 0) {
+        return cwd.replace(basePath, '')
+
+      }
+    }
+  }
+
+  const relPath = getRelPath()
+  if (!relPath) {
+    console.log(`Move to one of this base paths: ${chalk.red(basePaths)}`)
+  } else {
+    console.log(`Base path detected. The relative path is: ${chalk.yellow(relPath)}`)
+  }
+
+  for (const basePath of currentBasePaths) {
+    const absPath = path.join(basePath, relPath)
+    if (!fs.existsSync(absPath)) {
+      console.log(`Create directory: ${chalk.yellow(absPath)}`)
+      fs.mkdirSync(absPath, { recursive: true })
+    }
+    console.log(`Open directory: ${chalk.green(absPath)}`)
+    const process = childProcess.spawn('Thunar', [absPath], { detached: true })
+    process.unref()
+  }
+}
+
 commander
   .command('convert [input...]').alias('c')
   .option('-p, --preview-image', 'Convert into preview images (Smaller and different file name)')
   .description('Convert media files in the appropriate format. Multiple files, globbing works *.mp3')
   .action(convert)
+
+commander
+  .command('mirror').alias('m')
+  .description('Create and open in the file explorer a relative path in different base paths.')
+  .action(mirrorRelPath)
 
 commander
   .command('open').alias('o')
