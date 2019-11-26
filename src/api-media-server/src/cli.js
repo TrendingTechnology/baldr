2bcb7564b7462fd8a0bc7913a334a6180bb7fa57
Josef Friedrich <josef@friedrich.rocks>
2019-11-26 20:34:48 +0100

    Use again high efficency

--- src/api-media-server/src/cli.js
+++ src/api-media-server/src/cli.js
@@ -425,9 +425,9 @@ async function convertOneFile (inputFile, cmdObj) {
   if (assetType === 'audio') {
     convert = childProcess.spawn('ffmpeg', [
       '-i', inputFile,
-      '-c:a', 'aac', '-b:a', '128k',
+      // '-c:a', 'aac', '-b:a', '128k',
       // '-c:a', 'libfdk_aac', '-profile:a', 'aac_he', '-b:a', '64k',
-      // '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2',
+      '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2',
       '-vn', // Disable video recording
       '-map_metadata', '-1', // remove metadata
       '-y', // Overwrite output files without asking
