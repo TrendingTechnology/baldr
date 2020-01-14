2ab30849245e33821a4fa154e077360bfbc35a1a
Josef Friedrich <josef@friedrich.rocks>
2020-01-14 17:32:02 +0100

    Progress for the play button

--- src/vue-plugin-media/src/PlayButton.vue
+++ src/vue-plugin-media/src/PlayButton.vue
@@ -1,25 +1,58 @@
 <template>
-  <span class="vc_play_button">
-    <material-icon
-      v-if="started"
-      name="play-speed"
-      class="baldr-icon-spin"
-    />
-    <material-icon
-      v-if="!playing && !started"
-      name="play"
-      @click.native="start"
-    />
-    <material-icon
-      v-if="playing"
-      name="pause"
-      @click.native="$media.player.stop()"
-    />
-  </span>
+  <div class="vc_play_button">
+    <svg viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+      <g transform="translate(125,125)">
+        <circle r="100" class="circle-base"/>
+        <g transform="rotate(-90)">
+          <circle r="100" ref="progress" class="circle-progress"/>
+        </g>
+      </g>
+    </svg>
+    <div class="icons">
+
+      <plain-icon
+        v-if="status === 'starting'"
+        name="play-speed"
+        class="baldr-icon-spin"
+      />
+      <plain-icon
+        v-if="status === 'stopped'"
+        name="play"
+        @click.native="start"
+      />
+      <plain-icon
+        v-if="status === 'playing'"
+        name="play"
+        class="baldr-icon-spin"
+        @click.native="$media.player.stop()"
+      />
+
+
+      <!-- <plain-icon
+        v-if="started"
+        name="play-speed"
+        class="baldr-icon-spin"
+      />
+      <plain-icon
+        v-if="!playing && !started"
+        name="play"
+        @click.native="start"
+      />
+      <plain-icon
+        v-if="playing"
+        name="pause"
+        @click.native="$media.player.stop()"
+      /> -->
+    </div>
+  </div>
 </template>
 
 <script>
 import { MaterialIcon } from '@bldr/vue-plugin-material-icon'
+
+const circleRadius = 100;
+// 100%: 628.3185307179587
+const circumference = Math.PI * 2 * circleRadius
 
 export default {
   name: 'PlayButton',
@@ -33,27 +66,68 @@ export default {
   },
   data () {
     return {
-      started: false,
-      playing: false,
-      mediaFile: null
+      // starting
+      // playing
+      // stopped
+      status: 'stopped',
+      mediaElement: null
     }
   },
   methods: {
     start () {
-      this.started = true
+      this.status = 'starting'
       this.$media.player.load(this.sample)
       this.$media.player.start()
+    },
+    /**
+     * Set the progress on the first visible circle.
+     * Updates the stroke dash offset of the first visible circle
+     *
+     * @param {Number} progress - From 0 to 1.
+     */
+    setProgress (progress = 0.5) {
+      this.$refs.progress.style.strokeDashoffset = -(circumference * progress)
+    },
+    /**
+     * @param {currentTimeMsec} - The current time of the
+     *   corresponding media file in milliseconds.
+     */
+    calculateSampleProgress (mediaFileCurrentTimeSec) {
+      let sampleDurationSec
+      if (!this.sample.durationSec) {
+        // Samples without duration play until the end fo the media file.
+        sampleDurationSec = this.mediaElement.duration - this.sample.startTimeSec
+      } else {
+        sampleDurationSec = this.sample.durationSec
+      }
+
+      const sampleCurrentTimeSec = mediaFileCurrentTimeSec - this.sample.startTimeSec
+      // for example:
+      // current time: 6s duration: 60s
+      // 6 / 60 = 0.1
+      return sampleCurrentTimeSec / sampleDurationSec
     }
   },
   mounted () {
     this.mediaElement = this.sample.mediaElement
+
+    // Mount a playing media element.
+    if (!this.mediaElement.paused) {
+      this.status = 'playing'
+    }
+
+    this.mediaElement.ontimeupdate = (event) => {
+      if (!this.$refs.progress) return
+      const progress = this.calculateSampleProgress(event.target.currentTime)
+      this.setProgress(progress)
+    }
+
     this.mediaElement.onplay = (event) => {
-      this.playing = true
-      this.started = false
+      this.status = 'playing'
     }
 
     this.mediaElement.onpause = (event) => {
-      this.playing = false
+      this.status = 'stopped'
     }
   }
 }
@@ -62,5 +136,38 @@ export default {
 <style lang="scss" scoped>
   .vc_play_button {
     line-height: 1em;
+    width: 1em;
+    height: 1em;
+    position: relative;
+    display: inline-block;
+s
+    .icons {
+      width: 1em;
+      height: 1em;
+      left: 0;
+      position: absolute;
+      top: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .baldr-icon {
+        font-size: 0.7em;
+      }
+    }
+
+    circle {
+      stroke-width: 0.6em;
+      fill: none;
+    }
+
+    .circle-base {
+      stroke: $black;
+    }
+
+    .circle-progress {
+      stroke: $gray;
+      stroke-dasharray: 628.3185307179587;
+    }
   }
 </style>
