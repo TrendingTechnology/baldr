b5ef4406ff847f17850939ad437f1404cd845310
Josef Friedrich <josef@friedrich.rocks>
2020-03-04 11:48:17 +0100

    Fix alias

--- src/cli/src/commands/cloze.js
+++ src/cli/src/commands/cloze.js
@@ -78,7 +78,7 @@ function action (filePath) {
 
 module.exports = {
   command: 'cloze [input]',
-  alias: 'c',
+  alias: 'cl',
   checkExecutable: ['pdfinfo', 'pdf2svg', 'lualatex'],
   description: 'Generate from TeX files with cloze texts SVGs for baldr.',
   action
