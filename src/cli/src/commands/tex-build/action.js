976a3e670b9c367eafff714469edddebe7f2391a
Josef Friedrich <josef@friedrich.rocks>
2020-03-20 21:42:26 +0100

    use lualatex directly to build TeX files

--- src/cli/src/commands/tex-build/action.js
+++ src/cli/src/commands/tex-build/action.js
@@ -1,4 +1,5 @@
 // Node packages.
+const path = require('path')
 const childProcess = require('child_process')
 
 // Third party packages.
@@ -8,19 +9,15 @@ const chalk = require('chalk')
 const mediaServer = require('@bldr/api-media-server')
 
 function buildOneFile (filePath) {
-  // -cd Change to directory of source file when processing it
-  // -gg Super go mode: clean out generated files (-CA), and then process files
-  // regardless of file timestamps
-  let process = childProcess.spawnSync('latexmk', ['-cd', '-gg', '-lualatex', filePath])
+  let process = childProcess.spawnSync(
+    'lualatex', ['--halt-on-error', '--shell-escape', filePath],
+    { cwd: path.dirname(filePath) }
+  )
   if (process.status === 0) {
     console.log(chalk.green('OK') + ' ' + filePath)
   } else {
     console.log(chalk.red('ERROR') + ' ' + filePath)
   }
-
-  // -c clean up (remove) all nonessential files, except dvi, ps and pdf files.
-  // This and the other clean-ups are instead of a regular make.
-  process = childProcess.spawnSync('latexmk', ['-cd', '-c', filePath])
 }
 
 /**
