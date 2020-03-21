553b1d6883982d3520ae58ef14a925020d94f925
Josef Friedrich <josef@friedrich.rocks>
2020-03-21 16:40:26 +0100

    Question

--- src/vue-app-presentation/src/masters/question/Question.vue
+++ src/vue-app-presentation/src/masters/question/Question.vue
@@ -1,8 +1,8 @@
 <template>
   <div :class="['vc_question_master_question', `level-${q.level}`]">
     <h1 v-if="q.heading" v-html="q.heading"/>
-    <p class="question" v-if="q.question" v-html="q.question"/>
-    <p class="answer" v-if="q.answer" v-html="q.answer"/>
+    <p class="question" v-if="q.question" v-html="q.question" :id="`q${q.questionNo}`"/>
+    <p class="answer" v-if="q.answer" v-html="q.answer" :id="`a${q.answerNo}`"/>
     <ol v-if="q.subQuestions">
       <li
         v-for="question in q.subQuestions"
--- src/vue-app-presentation/src/masters/question/main.js
+++ src/vue-app-presentation/src/masters/question/main.js
@@ -1,16 +1,36 @@
 import { markupToHtml } from '@/lib.js'
 
 class Question {
-  constructor (spec, level) {
-    this.level = level
+
+  constructor (spec, counts) {
+    this.level = counts.level
     this.heading = null
     this.question = null
+    this.questionNo = null
     this.answer = null
+    this.answerNo = null
     this.subQuestions = null
 
+    const aliases = {
+      q: 'question',
+      a: 'answer',
+      h: 'heading',
+      s: 'subQuestions'
+    }
+
     if (typeof spec === 'string') {
+      counts.question++
+      this.questionNo = counts.question
+      counts.sequence.push(`q${counts.question}`)
       this.question = spec
     } else if (typeof spec === 'object') {
+      // aliases
+      for (const alias in aliases) {
+        if (spec.hasOwnProperty(alias)) {
+          spec[aliases[alias]] = spec[alias]
+          delete spec[alias]
+        }
+      }
       for (const prop of ['heading', 'question', 'answer']) {
         if (spec[prop]) {
           if (typeof spec[prop] === 'string') {
@@ -20,43 +40,58 @@ class Question {
           }
         }
       }
+      if (this.question) {
+        counts.question++
+        counts.sequence.push(`q${counts.question}`)
+        this.questionNo = counts.question
+      }
+      if (this.answer) {
+        counts.answer++
+        counts.sequence.push(`a${counts.answer}`)
+        this.answerNo = counts.answer
+      }
       if (spec.subQuestions) {
         this.subQuestions = []
-        Question.parseRecursively(spec.subQuestions, this.subQuestions, level + 1)
+        counts.level++
+        Question.parseRecursively(spec.subQuestions, this.subQuestions, counts)
       }
     }
   }
 
-  static parseRecursively (specs, processed = [], level = 0) {
+  static parseRecursively (specs, processed = [], counts) {
     if (Array.isArray(specs)) {
       for (const spec of specs) {
-        processed.push(new Question(spec, level))
+        processed.push(new Question(spec, counts))
       }
       return processed
     }
-    processed.push(new Question(specs, level))
+    processed.push(new Question(specs, counts))
     return processed
+  }
+
+  /**
+   * sequence: `['q1', 'a1', 'q2', 'q3']`
+   *
+   * @returns {Object}
+   */
+  static initCounts () {
+    return {
+      sequence: [],
+      question: 0,
+      answer: 0,
+      level: 0
+    }
   }
 }
 
 export default {
   title: 'Frage',
   props: {
-    heading: {
-      type: String,
-      description: 'Eine Überschrift, die über den Fragen angezeigt wird.',
-      markup: true
-    },
     questions: {
       type: Array,
       description: 'Eine Liste mit Objekten mit den Schlüsseln `question` and `answer`.',
       required: true,
       markup: true
-    },
-    numbers: {
-      type: Boolean,
-      description: 'Ob die Fragen nummeriert werden sollen.',
-      default: true
     }
   },
   icon: {
@@ -69,7 +104,8 @@ export default {
     darkMode: false
   },
   normalizeProps (props) {
-    const questions = Question.parseRecursively(props)
+    const counts = Question.initCounts()
+    const questions = Question.parseRecursively(props, [], counts)
     return { questions }
   }
 }
