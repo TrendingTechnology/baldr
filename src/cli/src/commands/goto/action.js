4744393850b76d8877cc747ab641aff1c47f6b75
Josef Friedrich <josef@friedrich.rocks>
2020-04-20 17:56:15 +0200

    Console log for go to

--- src/cli/src/commands/goto/action.js
+++ src/cli/src/commands/goto/action.js
@@ -1,6 +1,9 @@
 // Node packages.
 const childProcess = require('child_process')
 
+// Third party packages.
+const chalk = require('chalk')
+
 // Project packages.
 const mediaServer = require('@bldr/media-server')
 
@@ -10,6 +13,7 @@ const { cwd, config } = require('../../main.js')
 function action () {
   let mirroredPath = mediaServer.locationIndicator.getMirroredPath(cwd)
   if (!mirroredPath) mirroredPath = config.mediaServer.basePath
+  console.log(`Go to: ${chalk.green(mirroredPath)}`)
   childProcess.spawn('zsh', ['-i'], {
     cwd: mirroredPath,
     stdio: 'inherit'
