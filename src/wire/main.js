be7dda3aba557c374375ca34365b80b544bd9412
Josef Friedrich <josef@friedrich.rocks>
2020-04-26 20:06:17 +0200

    Add some docs

--- src/wire/main.js
+++ src/wire/main.js
@@ -3,15 +3,25 @@
 /**
  * A websocket server to connect multiple baldr lamp sessions.
  *
+ * @see {@link module:@bldr/lamp/remote-control}
+ * @see {@link https://www.npmjs.com/package/ws Package “ws” on npm}
+ * @see {@link https://github.com/websockets/ws/blob/master/doc/ws.md API documentation of the package “ws”}
+ *
  * @module @bldr/wire
  */
 
+// Third party packages.
 const WebSocket = require('ws')
 
+// Project packages.
 const core = require('@bldr/core-node')
 
+// Globals.
 const config = core.bootstrapConfig()
 
+/**
+ * Launch the web socket server.
+ */
 function main () {
   const wss = new WebSocket.Server({ port: config.wire.port })
 
