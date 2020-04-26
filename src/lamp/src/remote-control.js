5871949706f8f4afa88ea15cb60e05b6814ac4eb
Josef Friedrich <josef@friedrich.rocks>
2020-04-26 10:21:11 +0200

    Add file for docs

---
+++ src/lamp/src/remote-control.js
@@ -0,0 +1,20 @@
+/**
+ * In the speaker view all to the websocket connected BALDR Lamp
+ * instances are controlled.
+ *
+ * # Control message sender
+ *
+ * `this.$socket.sendObj()`
+ *
+ * # Listener
+ *
+ * in `MainApp.vue` in the lifecyle hook `mounted` the socket event listener
+ * `this.$options.sockets.onmessage` is bound to a function.
+ *
+ * # Used plugins:
+ *
+ * @see {@link module:@bldr/wire Web sockets server}
+ * @see {@link https://www.npmjs.com/package/vue-native-websocket Used Vue plugin “vue-native-websocket”}
+ *
+ * @module @bldr/lamp/remote-control
+ */
