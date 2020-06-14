fa7b782d46299477f53187002c830c47dfeb1ba4
Josef Friedrich <josef@friedrich.rocks>
2020-06-14 10:50:57 +0200

    Fix cloze

--- src/cli/src/commands/cloze/action.js
+++ src/cli/src/commands/cloze/action.js
@@ -72,7 +72,7 @@ function generateClozeSvg (filePath) {
  * Generate from TeX files with cloze texts SVGs for baldr.
  */
 function action (filePath) {
-  mediaServer.walkDeluxe(generateClozeSvg, new RegExp('.*\.tex$'), filePath) // eslint-disable-line
+  mediaServer.walk(generateClozeSvg, new RegExp('.*\.tex$'), filePath) // eslint-disable-line
 }
 
 module.exports = action
