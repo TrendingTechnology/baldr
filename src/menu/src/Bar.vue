f1e146927d4e7db9fb12fdbb343caae386eb2373
Josef Friedrich <josef@friedrich.rocks>
2020-09-30 21:14:58 +0200

    Fix ref undefined error in menu

--- src/menu/src/Bar.vue
+++ src/menu/src/Bar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bar">
-    <component v-for="(item, item_idx) in content"
+    <component v-for="(item, itemIndex) in content"
       :is="getComponent(item.is)"
-      :key="'bar-item-' + item_idx"
-      :ref="'bar-item-' + item_idx"
+      :key="'bar-item-' + itemIndex"
+      :ref="'bar-item-' + itemIndex"
       :item="item"
       :class="item.class"
       :is_open="isOpen"
-      @click="(event) => toggleMenu('bar-item-' + item_idx, event)" />
+      @click="(event) => toggleMenu('bar-item-' + itemIndex, event)" />
   </div>
 </template>
 
@@ -40,9 +40,9 @@ export default {
     toggleMenu(name, event) {
       event.stopPropagation();
       const ref = this.$refs[name][0];
-      const disabled = ref.item && ref.item.disabled;
+      const disabled = ref && ref.item && ref.item.disabled;
       const touch = event.sourceCapabilities && event.sourceCapabilities.firesTouchEvents;
-      this.isOpen = ref.isMenu && !disabled ? (touch ? true : !this.isOpen) : false;
+      this.isOpen = ref && ref.isMenu && !disabled ? (touch ? true : !this.isOpen) : false;
     },
     getComponent(is) {
       if(is && !Array.isArray(is) && typeof is == "object") return is;
