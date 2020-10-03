9c91bb94c534b3164156229e4b74ef5665dcf19c
Josef Friedrich <josef@friedrich.rocks>
2020-10-03 22:03:59 +0200

    Document preload.js

--- src/lamp/src/preload.js
+++ src/lamp/src/preload.js
@@ -1,3 +1,16 @@
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#preload-files
+/**
+ * Access some Electron Node objects using the global window object.
+ *
+ * Preload files allows to execute JS with Node integration in the context of
+ * your Vue App (shared window variable). This file is configured in
+ * ./background.js
+ *
+ * @see
+ * {@link https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#preload-files}
+ *
+ * @module @bldr/lamp/preload
+ */
+
 import { ipcRenderer } from 'electron'
+
 window.ipcRenderer = ipcRenderer
