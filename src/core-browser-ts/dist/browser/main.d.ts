952d18735521313cc4122c0b9acde49ef23b6074
Josef Friedrich <josef@friedrich.rocks>
2020-10-25 07:40:32 +0100

    Convert core-browser to typescript

--- src/core-browser-ts/dist/browser/main.d.ts
+++ src/core-browser-ts/dist/browser/main.d.ts
@@ -94,3 +94,219 @@ export declare const jsYamlConfig: {
  * `uuid:c262fe9b-c705-43fd-a5d4-4bb38178d9e7`
  */
 export declare const mediaUriRegExp: RegExp;
+/**
+ * Sleep some time
+ *
+ * @see {@link https://github.com/erikdubbelboer/node-sleep}
+ *
+ * @param milliSeconds
+ */
+export declare function msleep(milliSeconds: number): void;
+/**
+ * Create a deep copy of and object.
+ */
+export declare class RawDataObject {
+    /**
+     * The raw data object.
+     */
+    raw: {
+        [key: string]: any;
+    };
+    constructor(rawData: object);
+    /**
+     * Cut a property from the raw object, that means delete the property and
+     * return the value.
+     *
+     * @param property - The property of the object.
+     *
+     * @returns The data stored in the property
+     */
+    cut(property: string): any;
+    /**
+     * Assert if the raw data object is empty.
+     */
+    isEmpty(): boolean;
+}
+/**
+ * Escape some characters with HTML entities.
+ *
+ * @see {@link https://coderwall.com/p/ostduq/escape-html-with-javascript}
+ */
+export declare function escapeHtml(htmlString: string): string;
+/**
+ * Generate from the file name or the url of the first element of a
+ * multipart asset the nth file name or the url. The parameter
+ * `firstFileName` must have a extension (for example `.jpg`). The
+ * parameter `no` must be smaller then 100. Only two digit or smaller
+ * integers are allowed.
+ *
+ * 1. `multipart-asset.jpg`
+ * 2. `multipart-asset_no02.jpg`
+ * 3. `multipart-asset_no03.jpg`
+ * 4. ...
+ *
+ * @param firstFileName - A file name, a path or a URL.
+ * @param no - The number in the multipart asset list. The
+ *   first element has the number 1.
+ */
+export declare function formatMultiPartAssetFileName(firstFileName: string, no: string | number): string;
+/**
+ * Format a Wikidata URL.
+ * `https://www.wikidata.org/wiki/Q42`
+ */
+export declare function formatWikidataUrl(id: string): string;
+/**
+ * Format a Wikipedia URL.
+ *
+ * https://en.wikipedia.org/wiki/A_Article
+ *
+ * @param nameSpace - The name space of the Wikipedia article (for
+ *   example A_Article or en:A_article)
+ */
+export declare function formatWikipediaUrl(nameSpace: string): string;
+/**
+ * Format a Musicbrainz recording URL.
+ *
+ * `https://musicbrainz.org/recording/${RecordingId}`
+ *
+ * @param recordingId
+ */
+export declare function formatMusicbrainzRecordingUrl(recordingId: string): string;
+/**
+ * Format a Musicbrainz work URL.
+ *
+ * `https://musicbrainz.org/work/${WorkId}`
+ *
+ * @param workId
+ */
+export declare function formatMusicbrainzWorkUrl(workId: string): string;
+/**
+ * Format a YouTube URL.
+ *
+ * `https://youtu.be/CQYypFMTQcE`
+ *
+ * @param id - The id of a Youtube video (for example CQYypFMTQcE).
+ */
+export declare function formatYoutubeUrl(id: string): string;
+/**
+ * `https://imslp.org/wiki/La_clemenza_di_Tito_(Wagenseil,_Georg_Christoph)`
+ *
+ * @param id - For example
+ *   `La_clemenza_di_Tito_(Wagenseil,_Georg_Christoph)`
+ */
+export declare function formatImslpUrl(id: string): string;
+/**
+ * `https://commons.wikimedia.org/wiki/File:Cheetah_(Acinonyx_jubatus)_cub.jpg`
+ *
+ * @param fileName - For example
+ *   `Cheetah_(Acinonyx_jubatus)_cub.jpg`
+ */
+export declare function formatWikicommonsUrl(fileName: string): string;
+/**
+ * Get the plain text version of a HTML string.
+ *
+ * @param html - A HTML formated string.
+ *
+ * @returns The plain text version.
+ */
+export declare function plainText(html: string): string;
+interface ShortenTextOptions {
+    stripTags: boolean;
+    maxLength: number;
+}
+/**
+ * Shorten a text string. By default the string is shortend to the maximal
+ * length 80.
+ *
+ * @param text
+ * @param options
+ */
+export declare function shortenText(text: string, { maxLength, stripTags }: ShortenTextOptions): string;
+interface SelectionSubsetOption {
+    /**
+     * `numeric`, or a truety value.
+     */
+    sort: string | boolean;
+    /**
+     * An array of elements to build a subset
+     */
+    elements: any[];
+    /**
+     * If `elements` is undefined, an array with integers is created und
+     * used as `elements`.
+     */
+    elementsCount: number;
+    /**
+     *
+     */
+    firstElementNo: number;
+    /**
+     * Shift all selector numbers by this number: For example `-1`: `2-5`
+     *   is internally treated as `1-4`
+     */
+    shiftSelector: number;
+}
+/**
+ * Select a subset of elements by a string (`subsetSelector`). `1` is the first
+ * element of the `elements` array.
+ *
+ * @param subsetSelector - Select a subset of elements. Examples
+ *
+ * - `` (emtpy string or value which evalutes to false): All elements.
+ * - `1`: The first element.
+ * - `1,3,5`: The first, the third and the fifth element.
+ * - `1-3,5-7`: `1,2,3,5,6,7`
+ * - `-7`: All elements from the beginning up to `7` (`1-7`).
+ * - `7-`: All elements starting from `7` (`7-end`).
+ *
+ * @param options
+ *
+ * @returns {Array}
+ */
+export declare function selectSubset(subsetSelector: string, { sort, elements, elementsCount, firstElementNo, shiftSelector }: SelectionSubsetOption): any[];
+/**
+ * Sort alphabetically an array of objects by some specific properties.
+ *
+ * @param property - Key of the object to sort.
+ * @see {@link https://ourcodeworld.com/articles/read/764/how-to-sort-alphabetically-an-array-of-objects-by-key-in-javascript Tutorial}
+ */
+export declare function sortObjectsByProperty(property: string): (a: {
+    [key: string]: any;
+}, b: {
+    [key: string]: any;
+}) => any;
+/**
+ * Format a date specification string into a local date string, for
+ * example `28. August 1749`
+ *
+ * @param dateSpec - A valid input for the `Date()` class. If the input
+ *   is invalid the raw `dateSpec` is returned.
+ */
+export declare function formatToLocalDate(dateSpec: string): string;
+/**
+ * Extract the 4 digit year from a date string
+ *
+ * @param dateSpec - For example `1968-01-01`
+ *
+ * @returns for example `1968`
+ */
+export declare function formatToYear(dateSpec: string): string;
+/**
+ * Format a timestamp into a string like this example: `Mo 17.2.2020 07:57:53`
+ *
+ * @param timeStampMsec - The timestamp in milliseconds.
+ */
+export declare function formatToLocalDateTime(timeStampMsec: number): string;
+/**
+ * Convert a duration string (8:01 = 8 minutes 1 seconds or 1:33:12 = 1
+ * hour 33 minutes 12 seconds) into seconds.
+ *
+ * @param duration
+ */
+export declare function convertDurationToSeconds(duration: string): number;
+/**
+ * Convert a single word into title case, for example `word` gets `Word`.
+ *
+ * @param text
+ */
+export declare function toTitleCase(text: string): string;
--- src/core-browser-ts/dist/browser/main.js
+++ src/core-browser-ts/dist/browser/main.js
@@ -149,3 +149,408 @@ export const jsYamlConfig = {
  * `uuid:c262fe9b-c705-43fd-a5d4-4bb38178d9e7`
  */
 export const mediaUriRegExp = new RegExp('((id|uuid):(([a-zA-Z0-9-_]+)(#([a-zA-Z0-9-_]+))?))');
+/**
+ * Sleep some time
+ *
+ * @see {@link https://github.com/erikdubbelboer/node-sleep}
+ *
+ * @param milliSeconds
+ */
+export function msleep(milliSeconds) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, milliSeconds);
+}
+/**
+ * Create a deep copy of and object.
+ */
+export class RawDataObject {
+    constructor(rawData) {
+        this.raw = deepCopy(rawData);
+    }
+    /**
+     * Cut a property from the raw object, that means delete the property and
+     * return the value.
+     *
+     * @param property - The property of the object.
+     *
+     * @returns The data stored in the property
+     */
+    cut(property) {
+        if ({}.hasOwnProperty.call(this.raw, property)) {
+            const out = this.raw[property];
+            delete this.raw[property];
+            return out;
+        }
+    }
+    /**
+     * Assert if the raw data object is empty.
+     */
+    isEmpty() {
+        if (Object.keys(this.raw).length === 0)
+            return true;
+        return false;
+    }
+}
+/**
+ * Escape some characters with HTML entities.
+ *
+ * @see {@link https://coderwall.com/p/ostduq/escape-html-with-javascript}
+ */
+export function escapeHtml(htmlString) {
+    // List of HTML entities for escaping.
+    const htmlEscapes = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        '/': '&#x2F;'
+    };
+    // Regex containing the keys listed immediately above.
+    const htmlEscaper = /[&<>"'/]/g;
+    // Escape a string for HTML interpolation.
+    return ('' + htmlString).replace(htmlEscaper, function (match) {
+        return htmlEscapes[match];
+    });
+}
+/**
+ * Generate from the file name or the url of the first element of a
+ * multipart asset the nth file name or the url. The parameter
+ * `firstFileName` must have a extension (for example `.jpg`). The
+ * parameter `no` must be smaller then 100. Only two digit or smaller
+ * integers are allowed.
+ *
+ * 1. `multipart-asset.jpg`
+ * 2. `multipart-asset_no02.jpg`
+ * 3. `multipart-asset_no03.jpg`
+ * 4. ...
+ *
+ * @param firstFileName - A file name, a path or a URL.
+ * @param no - The number in the multipart asset list. The
+ *   first element has the number 1.
+ */
+export function formatMultiPartAssetFileName(firstFileName, no) {
+    if (!Number.isInteger(no)) {
+        // throw new Error(`${firstFileName}: The specifed number “${no}” is no integer.`)
+        no = 1;
+    }
+    let suffix;
+    if (no === 1) {
+        return firstFileName;
+    }
+    else if (no < 10) {
+        suffix = `_no00${no}`;
+    }
+    else if (no < 100) {
+        suffix = `_no0${no}`;
+    }
+    else if (no < 1000) {
+        suffix = `_no${no}`;
+    }
+    else {
+        throw new Error(`${firstFileName} multipart asset counts greater than 100 are not supported.`);
+    }
+    return firstFileName.replace(/(\.\w+$)/, `${suffix}$1`);
+}
+/**
+ * Format a Wikidata URL.
+ * `https://www.wikidata.org/wiki/Q42`
+ */
+export function formatWikidataUrl(id) {
+    id = String(id);
+    const idNumber = parseInt(id.replace(/^Q/, ''));
+    // https://www.wikidata.org/wiki/Q42
+    return `https://www.wikidata.org/wiki/Q${idNumber}`;
+}
+/**
+ * Format a Wikipedia URL.
+ *
+ * https://en.wikipedia.org/wiki/A_Article
+ *
+ * @param nameSpace - The name space of the Wikipedia article (for
+ *   example A_Article or en:A_article)
+ */
+export function formatWikipediaUrl(nameSpace) {
+    // https://de.wikipedia.org/wiki/Gesch%C3%BCtztes_Leerzeichen
+    // https://en.wikipedia.org/wiki/Non-breaking_space
+    const segments = nameSpace.split(':');
+    const lang = segments[0];
+    const slug = encodeURIComponent(segments[1]);
+    return `https://${lang}.wikipedia.org/wiki/${slug}`;
+}
+/**
+ * Format a Musicbrainz recording URL.
+ *
+ * `https://musicbrainz.org/recording/${RecordingId}`
+ *
+ * @param recordingId
+ */
+export function formatMusicbrainzRecordingUrl(recordingId) {
+    return `https://musicbrainz.org/recording/${recordingId}`;
+}
+/**
+ * Format a Musicbrainz work URL.
+ *
+ * `https://musicbrainz.org/work/${WorkId}`
+ *
+ * @param workId
+ */
+export function formatMusicbrainzWorkUrl(workId) {
+    return `https://musicbrainz.org/work/${workId}`;
+}
+/**
+ * Format a YouTube URL.
+ *
+ * `https://youtu.be/CQYypFMTQcE`
+ *
+ * @param id - The id of a Youtube video (for example CQYypFMTQcE).
+ */
+export function formatYoutubeUrl(id) {
+    return `https://youtu.be/${id}`;
+}
+/**
+ * `https://imslp.org/wiki/La_clemenza_di_Tito_(Wagenseil,_Georg_Christoph)`
+ *
+ * @param id - For example
+ *   `La_clemenza_di_Tito_(Wagenseil,_Georg_Christoph)`
+ */
+export function formatImslpUrl(id) {
+    return `https://imslp.org/wiki/${id}`;
+}
+/**
+ * `https://commons.wikimedia.org/wiki/File:Cheetah_(Acinonyx_jubatus)_cub.jpg`
+ *
+ * @param fileName - For example
+ *   `Cheetah_(Acinonyx_jubatus)_cub.jpg`
+ */
+export function formatWikicommonsUrl(fileName) {
+    return `https://commons.wikimedia.org/wiki/File:${fileName}`;
+}
+/**
+ * Get the plain text version of a HTML string.
+ *
+ * @param html - A HTML formated string.
+ *
+ * @returns The plain text version.
+ */
+export function plainText(html) {
+    if (!html)
+        return '';
+    // To get spaces between heading and paragraphs
+    html = html.replace(/></g, '> <');
+    const markup = new DOMParser().parseFromString(html, 'text/html');
+    return markup.body.textContent || '';
+}
+/**
+ * Shorten a text string. By default the string is shortend to the maximal
+ * length 80.
+ *
+ * @param text
+ * @param options
+ */
+export function shortenText(text, { maxLength, stripTags }) {
+    if (!text)
+        return '';
+    if (!maxLength)
+        maxLength = 80;
+    if (stripTags)
+        text = plainText(text);
+    if (text.length < maxLength)
+        return text;
+    // https://stackoverflow.com/a/5454303
+    // trim the string to the maximum length
+    text = text.substr(0, maxLength);
+    // re-trim if we are in the middle of a word
+    text = text.substr(0, Math.min(text.length, text.lastIndexOf(' ')));
+    return `${text} …`;
+}
+/**
+ * Select a subset of elements by a string (`subsetSelector`). `1` is the first
+ * element of the `elements` array.
+ *
+ * @param subsetSelector - Select a subset of elements. Examples
+ *
+ * - `` (emtpy string or value which evalutes to false): All elements.
+ * - `1`: The first element.
+ * - `1,3,5`: The first, the third and the fifth element.
+ * - `1-3,5-7`: `1,2,3,5,6,7`
+ * - `-7`: All elements from the beginning up to `7` (`1-7`).
+ * - `7-`: All elements starting from `7` (`7-end`).
+ *
+ * @param options
+ *
+ * @returns {Array}
+ */
+export function selectSubset(subsetSelector, { sort, elements, elementsCount, firstElementNo, shiftSelector }) {
+    const subset = [];
+    if (!shiftSelector)
+        shiftSelector = 0;
+    // Create elements
+    if (!elements && elementsCount) {
+        elements = [];
+        let firstNo;
+        if (firstElementNo) {
+            firstNo = firstElementNo;
+        }
+        else {
+            firstNo = 0;
+        }
+        const endNo = firstNo + elementsCount;
+        for (let i = firstNo; i < endNo; i++) {
+            elements.push(i);
+        }
+    }
+    if (!subsetSelector)
+        return elements;
+    // 1, 3, 5 -> 1,3,5
+    subsetSelector = subsetSelector.replace(/\s*/g, '');
+    // 1-3,5-7
+    const ranges = subsetSelector.split(',');
+    // for cloze steps: shiftSelector = -1
+    // shiftSelectorAdjust = 1
+    const shiftSelectorAdjust = -1 * shiftSelector;
+    for (let range of ranges) {
+        // -7 -> 1-7
+        if (range.match(/^-/)) {
+            const end = parseInt(range.replace('-', ''));
+            range = `${1 + shiftSelectorAdjust}-${end}`;
+        }
+        // 7- -> 7-23
+        if (range.match(/-$/)) {
+            const begin = parseInt(range.replace('-', ''));
+            // for cloze steps (shiftSelector: -1): 7- -> 7-23 -> elements.length
+            // as 22 elements because 7-23 translates to 6-22.
+            range = `${begin}-${elements.length + shiftSelectorAdjust}`;
+        }
+        const rangeSplit = range.split('-');
+        let startEnd;
+        if (rangeSplit.length === 2) {
+            startEnd = [parseInt(rangeSplit[0]), parseInt(rangeSplit[1])];
+        }
+        else {
+            startEnd = [parseInt(rangeSplit[0])];
+        }
+        // 1
+        if (startEnd.length === 1) {
+            const i = startEnd[0];
+            subset.push(elements[i - 1 + shiftSelector]);
+            // 1-3
+        }
+        else if (startEnd.length === 2) {
+            const beginNo = startEnd[0] + shiftSelector;
+            const endNo = startEnd[1] + shiftSelector;
+            if (endNo <= beginNo) {
+                throw new Error(`Invalid range: ${beginNo}-${endNo}`);
+            }
+            for (let no = beginNo; no <= endNo; no++) {
+                const index = no - 1;
+                subset.push(elements[index]);
+            }
+        }
+    }
+    if (sort === 'numeric') {
+        subset.sort((a, b) => a - b); // For ascending sort
+    }
+    else if (sort) {
+        subset.sort();
+    }
+    return subset;
+}
+/**
+ * Sort alphabetically an array of objects by some specific properties.
+ *
+ * @param property - Key of the object to sort.
+ * @see {@link https://ourcodeworld.com/articles/read/764/how-to-sort-alphabetically-an-array-of-objects-by-key-in-javascript Tutorial}
+ */
+export function sortObjectsByProperty(property) {
+    return function (a, b) {
+        return a[property].localeCompare(b[property]);
+    };
+}
+/**
+ * Format a date specification string into a local date string, for
+ * example `28. August 1749`
+ *
+ * @param dateSpec - A valid input for the `Date()` class. If the input
+ *   is invalid the raw `dateSpec` is returned.
+ */
+export function formatToLocalDate(dateSpec) {
+    const date = new Date(dateSpec);
+    // Invalid date
+    if (isNaN(date.getDay()))
+        return dateSpec;
+    const months = [
+        'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
+    ];
+    // Not getDay()
+    return `${date.getDate()}. ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+/**
+ * Extract the 4 digit year from a date string
+ *
+ * @param dateSpec - For example `1968-01-01`
+ *
+ * @returns for example `1968`
+ */
+export function formatToYear(dateSpec) {
+    return dateSpec.substr(0, 4);
+}
+/**
+ * Format a timestamp into a string like this example: `Mo 17.2.2020 07:57:53`
+ *
+ * @param timeStampMsec - The timestamp in milliseconds.
+ */
+export function formatToLocalDateTime(timeStampMsec) {
+    const date = new Date(Number(timeStampMsec));
+    const dayNumber = date.getDay();
+    let dayString;
+    if (dayNumber === 0) {
+        dayString = 'So';
+    }
+    else if (dayNumber === 1) {
+        dayString = 'Mo';
+    }
+    else if (dayNumber === 2) {
+        dayString = 'Di';
+    }
+    else if (dayNumber === 3) {
+        dayString = 'Mi';
+    }
+    else if (dayNumber === 4) {
+        dayString = 'Do';
+    }
+    else if (dayNumber === 5) {
+        dayString = 'Fr';
+    }
+    else if (dayNumber === 6) {
+        dayString = 'Sa';
+    }
+    const dateString = date.toLocaleDateString();
+    const timeString = date.toLocaleTimeString();
+    return `${dayString} ${dateString} ${timeString}`;
+}
+/**
+ * Convert a duration string (8:01 = 8 minutes 1 seconds or 1:33:12 = 1
+ * hour 33 minutes 12 seconds) into seconds.
+ *
+ * @param duration
+ */
+export function convertDurationToSeconds(duration) {
+    if (typeof duration === 'string' && duration.match(/:/)) {
+        const segments = duration.split(':');
+        if (segments.length === 3) {
+            return parseInt(segments[0]) * 3600 + parseInt(segments[1]) * 60 + parseInt(segments[2]);
+        }
+        else if (segments.length === 2) {
+            return parseInt(segments[0]) * 60 + parseInt(segments[1]);
+        }
+    }
+    return Number.parseFloat(duration);
+}
+/**
+ * Convert a single word into title case, for example `word` gets `Word`.
+ *
+ * @param text
+ */
+export function toTitleCase(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
