8a5c59c4a0c3e4ef0a5817983ea21d4eda373bf1
Josef Friedrich <josef@friedrich.rocks>
2021-01-23 11:34:39 +0100

    New cli command: video-cut

---
+++ src/mgmt/cli/dist/node/commands/video-cut/action.js
@@ -0,0 +1,31 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// Project packages.
+const cli_utils_1 = require("@bldr/cli-utils");
+const core_browser_1 = require("@bldr/core-browser");
+function action(videoFilePath, time1, time2, cmdObj) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const cmd = new cli_utils_1.CommandRunner({ verbose: true });
+        let startSec = 0;
+        let endSec;
+        if (!time2) {
+            endSec = core_browser_1.convertDurationToSeconds(time1);
+        }
+        else {
+            startSec = core_browser_1.convertDurationToSeconds(time1);
+            endSec = core_browser_1.convertDurationToSeconds(time2);
+        }
+        cmd.startSpin();
+        yield cmd.exec(['MP4Box', '-splitx', `${startSec}:${endSec}`, videoFilePath]);
+        cmd.stopSpin();
+    });
+}
+module.exports = action;
---
+++ src/mgmt/cli/src/commands/video-cut/action.ts
@@ -0,0 +1,23 @@
+// Project packages.
+import { CommandRunner } from '@bldr/cli-utils'
+
+import { convertDurationToSeconds } from '@bldr/core-browser'
+
+async function action (videoFilePath: string, time1: string, time2: string, cmdObj: { [key: string]: any }): Promise<void> {
+  const cmd = new CommandRunner({ verbose: true })
+
+  let startSec = 0;
+  let endSec;
+  if (!time2) {
+    endSec = convertDurationToSeconds(time1);
+  } else {
+    startSec = convertDurationToSeconds(time1);
+    endSec = convertDurationToSeconds(time2);
+  }
+
+  cmd.startSpin()
+  await cmd.exec(['MP4Box', '-splitx', `${startSec}:${endSec}` , videoFilePath])
+  cmd.stopSpin()
+}
+
+export = action
