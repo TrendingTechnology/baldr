e174878e393e0bb2f6cd5e47f9ca0218d2aec89f
Josef Friedrich <josef@friedrich.rocks>
2021-04-24 17:17:56 +0200

    Add test

--- src/lib/node/media-categories/tests/main.js
+++ src/lib/node/media-categories/tests/main.js
@@ -4,9 +4,16 @@ const { categoriesManagement } = require('../dist/node/main.js')
 
 describe('Package “@bldr/media-categories”', function () {
 
-  it('{ propertyOne: 1 }', function () {
-    const result = categoriesManagement.process({ id: 'test', title: 'test' })
-    assert.strictEqual(result.id, '---\nproperty_one: 1\n')
+  it('categoriesManagement.process()', function () {
+    const result = categoriesManagement.process({
+      id: 'test',
+      title: 'Test',
+      filePath: '/home/10_Presentation/NB/score.jpg',
+      extension: 'jpg'
+    })
+    assert.strictEqual(result.id, 'Presentation_NB_test')
+    assert.strictEqual(result.uuid.length, 36)
+    assert.ok(result.filePath == null)
+    assert.ok(result.extension == null)
   })
-
 })
