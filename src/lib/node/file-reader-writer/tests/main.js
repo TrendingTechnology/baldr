f32ad871f0425dab1967d482a9406785bffe5c4b
Josef Friedrich <josef@friedrich.rocks>
2021-05-18 21:04:21 +0200

    File read writer tests

--- src/lib/node/file-reader-writer/tests/main.js
+++ src/lib/node/file-reader-writer/tests/main.js
@@ -1,16 +1,33 @@
 /* globals describe it */
 
-const { readFile, readJsonFile } = require('../dist/node/main.js')
+const file = require('../dist/node/main.js')
 const assert = require('assert')
 
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+
+const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'bldr-file-reader-writer'))
+const tmpFile = path.join(tmpDir, 'test.txt')
+
 describe('Package “@bldr/file-reader-writer”', function () {
-  it('Function “readFile()”', function () {
-    const content = readFile('/etc/baldr.json')
-    assert.ok(content.indexOf('url') > -1)
+  it('Functions “writeFile()” and “readFile()”', function () {
+    file.writeFile(tmpFile, 'test')
+    const content = file.readFile(tmpFile)
+    assert.strictEqual(content, 'test')
   })
 
-  it('Function “readJsonFile()”', function () {
-    const content = readJsonFile('/etc/baldr.json')
-    assert.strictEqual(content.api.port, '44523')
+  it('Functions “writeJsonFile()” and  “readJsonFile()”', function () {
+    file.writeJsonFile(tmpFile, { test: 'test' })
+    const content = file.readJsonFile(tmpFile)
+    assert.strictEqual(content.test, 'test')
+  })
+
+  it('Functions “writeYamlFile()” and  “readYamlFile()”', function () {
+    file.writeYamlFile(tmpFile, { camelCase: { snakeCase: 'test' } })
+    const txtContent = file.readFile(tmpFile)
+    assert.ok(txtContent.indexOf('snake_case') > -1)
+    const content = file.readYamlFile(tmpFile)
+    assert.strictEqual(content.camelCase.snakeCase, 'test')
   })
 })
