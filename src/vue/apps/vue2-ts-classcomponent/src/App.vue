960949b9afc55c3b9d9b5e0baafc144a2ad671e6
Josef Friedrich <josef@friedrich.rocks>
2021-06-26 19:36:01 +0200

    Test

--- src/vue/apps/vue2-ts-classcomponent/src/App.vue
+++ src/vue/apps/vue2-ts-classcomponent/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <HelloWorld msg="Vue 2 Typescript Class Component Test App"/>
+    <javascript-test/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
+import JavascriptTest from './components/JavascriptTest.vue'
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld, JavascriptTest
   }
 })
 export default class App extends Vue {}
---
+++ src/vue/apps/vue2-ts-classcomponent/src/components/JavascriptTest.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Javascript Test Component</h1>
+
+    <p @click="increment">{{ counter }}</p>
+  </div>
+</template>
+
+<script lang="js">
+/* eslint-disable */
+
+import { increment } from '@/include-javascript.js'
+
+export default {
+  name: 'CursorArrows',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.counter = increment(this.counter)
+      return null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
