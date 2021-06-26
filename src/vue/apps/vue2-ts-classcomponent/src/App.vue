034c7a32e906bc52ab332d4415ac70c76ef48ad7
Josef Friedrich <josef@friedrich.rocks>
2021-06-26 19:26:35 +0200

    Add typescript vue test app

---
+++ src/vue/apps/vue2-ts-classcomponent/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Vue 2 Typescript Class Component Test App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/vue/apps/vue2-ts-classcomponent/src/include-javascript.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+export function increment (a) {
+  return ++a;
+}
