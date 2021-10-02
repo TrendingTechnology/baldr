4a3ff2bf4fffe77f163e770aa094b2945f9c4b76
Josef Friedrich <josef@friedrich.rocks>
2021-10-02 17:29:33 +0200

    mp3 to mp4

--- src/cli/src/commands/m4a-to-mp3/action.ts
+++ src/cli/src/commands/m4a-to-mp3/action.ts
@@ -1,16 +1,20 @@
 import fs from 'fs'
 
+import * as log from '@bldr/log'
+
 async function action (): Promise<void> {
   const files = fs.readdirSync(process.cwd())
 
   for (const file of files) {
-    console.log(file)
     if (file.match(/\.m4a$/) != null) {
       fs.unlinkSync(file)
+      log.always('Delete %s', file)
     }
 
     if (file.match(/\.m4a\./) != null) {
-      fs.renameSync(file, file.replace('.m4a.', '.mp3.'))
+      const newFile = file.replace('.m4a.', '.mp3.')
+      fs.renameSync(file, newFile)
+      log.always('Rename from %s to %s', file, newFile)
     }
   }
 }
