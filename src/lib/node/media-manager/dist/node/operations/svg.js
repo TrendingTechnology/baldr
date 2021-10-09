c903d6abc3a8ba7a075f32d369ad3abc556325f8
Josef Friedrich <josef@friedrich.rocks>
2021-10-09 20:22:20 +0200

    SVG

--- src/lib/node/media-manager/dist/node/operations/svg.js
+++ src/lib/node/media-manager/dist/node/operations/svg.js
@@ -4,9 +4,8 @@ exports.removeWidthHeightInSvg = void 0;
 const file_reader_writer_1 = require("@bldr/file-reader-writer");
 function removeWidthHeightInSvg(filePath) {
     let content = (0, file_reader_writer_1.readFile)(filePath);
-    content = content.replace(/<svg.*?>/si, function (substring) {
-        substring = substring.replace(/\s*(height|width)\s*=\s*".*?"\s*\n*/ig, ' ');
-        substring = substring.replace(/  +/g, ' ');
+    content = content.replace(/<svg.*?>/is, function (substring) {
+        substring = substring.replace(/\n*\s*(height|width)\s*=\s*".*?"/gi, '');
         return substring;
     });
     (0, file_reader_writer_1.writeFile)(filePath, content);
