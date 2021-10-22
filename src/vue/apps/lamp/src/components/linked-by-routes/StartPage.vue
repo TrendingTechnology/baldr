95e89b37de6756336439ca82b84d19dbcf03df6b
Josef Friedrich <josef@friedrich.rocks>
2021-10-22 20:29:50 +0200

    Redesign Start page

--- src/vue/apps/lamp/src/components/linked-by-routes/StartPage.vue
+++ src/vue/apps/lamp/src/components/linked-by-routes/StartPage.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="vc_start_page main-app-padding main-app-fullscreen" b-ui-theme="default">
-
-    <drop-down-menu v-if="!$isElectron"/>
+  <div
+    class="vc_start_page main-app-padding main-app-fullscreen"
+    b-ui-theme="default"
+  >
+    <drop-down-menu v-if="!$isElectron" />
 
     <div class="top-icon">
-      <div class="logo"><material-icon color="red" name="baldr"/>Baldr</div>
-      <div class="subtitle">Software im Browser für den Musikunterricht</div>
+      <div class="logo"><material-icon color="red" name="baldr" />Baldr</div>
+      <div class="subtitle">Präsentations-Software für den Schuleinsatz</div>
     </div>
 
-    <open-interface/>
+    <open-interface />
 
-    <recent-presentations/>
+    <recent-presentations />
   </div>
 </template>
 
@@ -38,34 +40,35 @@ export default class StartPage extends Vue {
 </script>
 
 <style lang="scss">
-  .vc_start_page {
-    font-size: 2vw;
-    display: table;
+.vc_start_page {
+  display: table;
+  font-size: 2vw;
 
-    .vc_open_interface {
-      display: table-cell;
-      vertical-align: middle;
-    }
+  .vc_open_interface {
+    display: table-cell;
+    vertical-align: middle;
+  }
 
-    .top-icon {
-      width: 100%;
-      left: 0vw;
-      position: absolute;
-      text-align: center;
-      top: 5vw;
+  .top-icon {
+    left: 0vw;
+    position: absolute;
+    text-align: center;
+    top: 5vw;
+    width: 100%;
 
-      .logo {
-        font-size: 10vw;
-        font-family: $font-family-sans-small-caps;
-        color: $red;
-      }
-    }
-
-    .vc_drop_down_menu {
-      font-size: 0.8em;
-      left: 1vw;
-      position: absolute;
-      top: 1vw;
+    .logo {
+      color: $red;
+      font-family: $font-family-sans-small-caps;
+      font-size: 10vw;
+      font-weight: bold;
     }
   }
+
+  .vc_drop_down_menu {
+    font-size: 0.8em;
+    left: 1vw;
+    position: absolute;
+    top: 1vw;
+  }
+}
 </style>
