1bd890d8274fba2bf18f656a1efeaa7326b9a132
Josef Friedrich <josef@friedrich.rocks>
2021-11-15 18:04:52 +0100

    Selector

--- src/lib/browser/dom-manipulator/tests/test-selector.js
+++ src/lib/browser/dom-manipulator/tests/test-selector.js
@@ -1,18 +1,64 @@
 /* globals describe it */
 
 const assert = require('assert')
+const path = require('path')
 
-const { ElementSelector } = require('../dist/node/selector.js')
+const {
+  ElementSelector,
+  InkscapeSelector
+} = require('../dist/node/selector.js')
+const { readFile } = require('@bldr/file-reader-writer')
+const { getConfig } = require('@bldr/config-ng')
+const config = getConfig()
+
+function readMediaFile (relPath) {
+  return readFile(path.join(config.mediaServer.basePath, relPath))
+}
 
 describe('Class “ElementSelector()”', function () {
   it('Method select(): <p>test</p>', function () {
     const selector = new ElementSelector('<p>test</p>', 'p')
     const steps = selector.select()
-    assert.strictEqual(steps[0].htmlElement.constructor.name, 'HTMLParagraphElement')
+    assert.strictEqual(
+      steps[0].htmlElement.constructor.name,
+      'HTMLParagraphElement'
+    )
   })
 
   it('Method count(): <p>test</p>', function () {
     const selector = new ElementSelector('<p>test</p>', 'p')
     assert.strictEqual(selector.count(), 2)
+  })
+})
+
+describe('Class “InkscapeSelector()”', function () {
+  const inventioString = readMediaFile(
+    'Musik/06/20_Mensch-Zeit/10_Bach/30_Invention/NB/Invention_C-Dur_Loesung.svg'
+  )
+  describe('Invention_C-Dur_Loesung.svg', function () {
+    it('layer', function () {
+      const selector = new InkscapeSelector(inventioString, 'layer')
+      assert.strictEqual(selector.count(), 5)
+    })
+
+    it('layer+', function () {
+      const selector = new InkscapeSelector(inventioString, 'layer+')
+      assert.strictEqual(selector.count(), 49)
+    })
+
+    it('group', function () {
+      const selector = new InkscapeSelector(inventioString, 'group')
+      assert.strictEqual(selector.count(), 1218)
+    })
+  })
+
+  it('10_Schoenberg-Moses/NB/Notationsweisen.svg', function () {
+    const selector = new InkscapeSelector(
+      readMediaFile(
+        'Musik/11/10_Sprache/30_20-Jahrhundert/10_Schoenberg-Moses/NB/Notationsweisen.svg'
+      ),
+      'group'
+    )
+    assert.strictEqual(selector.count(), 5)
   })
 })
--- src/lib/node/universal-dom/tests/test-main.js
+++ src/lib/node/universal-dom/tests/test-main.js
@@ -1,13 +1,30 @@
 /* globals describe it */
 const assert = require('assert')
+const path = require('path')
 
 const { DOMParserU, documentU } = require('../dist/node/node-main.js')
+const { readFile } = require('@bldr/file-reader-writer')
+const { getConfig } = require('@bldr/config-ng')
+const config = getConfig()
 
 describe('Package “@bldr/universal-dom”', function () {
-  it('DOMParserU', function () {
-    const dom = new DOMParserU().parseFromString('<p>test</p>', 'text/html')
-    const p = dom.querySelector('p')
-    assert.strictEqual(p.textContent, 'test')
+  describe('DOMParserU', function () {
+    it('html', function () {
+      const dom = new DOMParserU().parseFromString('<p>test</p>', 'text/html')
+      const p = dom.querySelector('p')
+      assert.strictEqual(p.textContent, 'test')
+    })
+
+    it('svg', function () {
+      const svgDom = new DOMParserU().parseFromString(
+        readFile(
+          path.join(config.mediaServer.basePath, 'Musik/06/20_Mensch-Zeit/10_Bach/30_Invention/NB/Invention_C-Dur_Loesung.svg')
+        ),
+        'image/svg+xml'
+      )
+      assert.strictEqual(svgDom.querySelectorAll('svg g').length, 1217)
+      // Musik/11/10_Sprache/30_20-Jahrhundert/10_Schoenberg-Moses/NB/Notationsweisen.svg
+    })
   })
 
   it('documentU', function () {
