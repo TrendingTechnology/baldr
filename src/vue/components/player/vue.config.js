3f207e206cd64f668a438b2016dc1647dbb7e099
Josef Friedrich <josef@friedrich.rocks>
2021-11-30 16:09:38 +0100

    Load config

--- src/vue/components/player/vue.config.js
+++ src/vue/components/player/vue.config.js
@@ -1,5 +1,15 @@
+const { getConfig } = require('@bldr/config')
+const { DefinePlugin } = require('webpack')
+
+const config = getConfig()
+
 module.exports = {
   configureWebpack: {
+    plugins: [
+      new DefinePlugin({
+        config: JSON.stringify(config),
+      })
+    ],
     entry: {
       app: './src/app.ts'
     }
