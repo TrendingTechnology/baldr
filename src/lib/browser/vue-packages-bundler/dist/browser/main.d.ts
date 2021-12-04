1d4aa958b7523b57d48dbae49584d86bb31ea6bc
Josef Friedrich <josef@friedrich.rocks>
2021-12-04 20:42:36 +0100

    Remove unused export

--- src/lib/browser/vue-packages-bundler/dist/browser/main.d.ts
+++ src/lib/browser/vue-packages-bundler/dist/browser/main.d.ts
@@ -2,6 +2,4 @@ export { CreateElement, VNode, VNodeData } from 'vue';
 export { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 export { RouteConfig } from 'vue-router';
 import VueRouterDefault from 'vue-router';
-import VueComponentDefault from 'vue';
 export declare const VueRouter: typeof VueRouterDefault;
-export declare const VueComponent: import("vue").VueConstructor<VueComponentDefault>;
--- src/lib/browser/vue-packages-bundler/dist/browser/main.js
+++ src/lib/browser/vue-packages-bundler/dist/browser/main.js
@@ -1,5 +1,3 @@
 export { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 import VueRouterDefault from 'vue-router';
-import VueComponentDefault from 'vue';
 export const VueRouter = VueRouterDefault;
-export const VueComponent = VueComponentDefault;
