05298cbd840f968e197b563c546378903ba17ee2
Josef Friedrich <josef@friedrich.rocks>
2021-12-21 15:58:01 +0100

    Clean up background.ts

--- src/vue/apps/lamp/src/background.ts
+++ src/vue/apps/lamp/src/background.ts
@@ -3,9 +3,6 @@
  *
  * @module @bldr/lamp/background
  */
-
-'use strict'
-
 import { app, protocol, BrowserWindow, Menu, shell } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
@@ -16,11 +13,8 @@ import { getEletronMenuDef } from '@bldr/menu-adapter'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win: BrowserWindow | null
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
@@ -29,12 +23,10 @@ contextMenu({
   prepend: (defaultActions, params, browserWindow) => [
     {
       label: 'Rainbow',
-      // Only show it when right-clicking images
       visible: params.mediaType === 'image'
     },
     {
       label: 'Search Google for “{selection}”',
-      // Only show it when right-clicking text
       visible: params.selectionText.trim().length > 0,
       click: () => {
         shell.openExternal(
@@ -50,14 +42,11 @@ contextMenu({
 const disposeContextMenu = contextMenu()
 
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: false,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION != null,
       webSecurity: false,
       allowRunningInsecureContent: true,
@@ -67,12 +56,10 @@ function createWindow () {
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
-    // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
 
@@ -84,29 +71,20 @@ function createWindow () {
   disposeContextMenu()
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
@@ -116,7 +94,6 @@ app.on('ready', async () => {
   createWindow()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', data => {
