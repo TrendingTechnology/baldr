0fc5e98d3d564d6665fc0c77b87fee688301a602
Josef Friedrich <josef@friedrich.rocks>
2021-12-28 17:09:51 +0100

    Test slide view

---
+++ src/vue/apps/lamp/tests/e2e/specs/slide-view.js
@@ -0,0 +1,25 @@
+describe('Increase and decrease font size', () => {
+  it('Increase', () => {
+    cy.visit('/#/presentation/EP_master_generic/slide/1')
+    cy.contains('Lorem')
+    cy.get('.vc_slide_view').should('have.css', 'font-size', '20px')
+    cy.get('body').type('{ctrl}2')
+    cy.get('.vc_slide_view').should('have.css', 'font-size', '21px')
+  })
+
+  it('Decrease', () => {
+    cy.visit('/#/presentation/EP_master_generic/slide/1')
+    cy.contains('Lorem')
+    cy.get('body').type('{ctrl}3')
+    cy.get('.vc_slide_view').should('have.css', 'font-size', '19px')
+  })
+
+  it.only('Reset', () => {
+    cy.visit('/#/presentation/EP_master_generic/slide/1')
+    cy.contains('Lorem')
+    cy.get('body').type('{ctrl}2')
+    cy.get('.vc_slide_view').should('have.css', 'font-size', '21px')
+    cy.get('body').type('{ctrl}1')
+    cy.get('.vc_slide_view').should('have.css', 'font-size', '20px')
+  })
+})
