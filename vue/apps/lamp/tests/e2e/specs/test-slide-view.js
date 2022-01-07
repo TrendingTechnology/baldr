93962aab9a2845bda474e278ec5a8534724bfdc5
Josef Friedrich <josef@friedrich.rocks>
2022-01-07 17:42:01 +0100

    Fix e2e tests

--- vue/apps/lamp/tests/e2e/specs/test-slide-view.js
+++ vue/apps/lamp/tests/e2e/specs/test-slide-view.js
@@ -3,21 +3,21 @@ describe('Increase and decrease font size', () => {
     cy.visit('/presentation/EP_master_generic/slide/1')
     cy.contains('Lorem')
     cy.get('.vc_slide_view').should('have.css', 'font-size', '20px')
-    cy.get('body').type('{ctrl}2')
+    cy.get('body').type('+')
     cy.get('.vc_slide_view').should('have.css', 'font-size', '21px')
   })
 
-  it('Shortcut to dncrease font size', () => {
+  it('Shortcut to decrease font size', () => {
     cy.visit('/presentation/EP_master_generic/slide/1')
     cy.contains('Lorem')
-    cy.get('body').type('{ctrl}3')
+    cy.get('body').type('-')
     cy.get('.vc_slide_view').should('have.css', 'font-size', '19px')
   })
 
   it('Shortcut to reset font size', () => {
     cy.visit('/presentation/EP_master_generic/slide/1')
     cy.contains('Lorem')
-    cy.get('body').type('{ctrl}2')
+    cy.get('body').type('+')
     cy.get('.vc_slide_view').should('have.css', 'font-size', '21px')
     cy.get('body').type('{ctrl}1')
     cy.get('.vc_slide_view').should('have.css', 'font-size', '20px')
