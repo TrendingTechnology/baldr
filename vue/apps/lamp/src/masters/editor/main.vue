fab7097f7142d6aa9209588d4ca3aa26d2b065a8
Josef Friedrich <josef@friedrich.rocks>
2022-01-21 14:51:58 +0100

    Buttons for the editor

--- vue/apps/lamp/src/masters/editor/main.vue
+++ vue/apps/lamp/src/masters/editor/main.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="vc_editor_master" spellcheck="false" v-html="markupSafe"></div>
+  <div class="vc_editor_master" spellcheck="false">
+    <div class="editor-toolbar gradually-appear" b-ui-theme="default">
+      <clickable-icon @click.native="formatHeading1" name="editor-heading-1" />
+      <clickable-icon @click.native="formatHeading2" name="editor-heading-2" />
+      <clickable-icon @click.native="formatHeading3" name="editor-heading-3" />
+      <clickable-icon @click.native="formatBold" name="editor-bold" />
+      <clickable-icon @click.native="formatUnderline" name="editor-underline" />
+      <clickable-icon @click.native="formatList" name="editor-list" />
+    </div>
+    <div v-html="markupSafe"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -38,10 +48,34 @@ export default class EditorMasterMain extends MasterMain {
     }
   }
 
-  insertHtml (html: string): void {
+  private insertHtml (html: string): void {
     const range = document.getSelection().getRangeAt(0)
     const fragment = document.createRange().createContextualFragment(html)
     range.insertNode(fragment)
+  }
+
+  formatHeading1 (): void {
+    this.surround('h1')
+  }
+
+  formatHeading2 (): void {
+    this.surround('h2')
+  }
+
+  formatHeading3 (): void {
+    this.surround('h3')
+  }
+
+  formatBold (): void {
+    this.surround('strong')
+  }
+
+  formatUnderline (): void {
+    this.surround('u')
+  }
+
+  formatList (): void {
+    this.insertHtml('<ul><li>.</li></ul>')
   }
 
   created (): void {
@@ -50,19 +84,22 @@ export default class EditorMasterMain extends MasterMain {
     document.addEventListener('keydown', event => {
       if (event.ctrlKey && event.key === 'b') {
         event.preventDefault()
-        this.surround('strong')
+        this.formatBold()
       } else if (event.ctrlKey && event.key === 'u') {
         event.preventDefault()
-        this.surround('u')
+        this.formatUnderline()
       } else if (event.ctrlKey && event.key === 'l') {
         event.preventDefault()
         this.insertHtml('<ul><li>.</li></ul>')
       } else if (event.ctrlKey && event.key === '1') {
         event.preventDefault()
-        this.surround('h1')
+        this.formatHeading1()
       } else if (event.ctrlKey && event.key === '2') {
         event.preventDefault()
-        this.surround('h2')
+        this.formatHeading2()
+      } else if (event.ctrlKey && event.key === '3') {
+        event.preventDefault()
+        this.formatHeading3()
       }
     })
   }
@@ -94,6 +131,18 @@ export default class EditorMasterMain extends MasterMain {
 .vc_editor_master {
   // left right padding more because of ul ol etc ...
   padding: 1vw 4vw;
+
+  .editor-toolbar {
+    border: 1px solid $gray;
+    position: absolute;
+    right: 0em;
+    top: 0em;
+
+    .baldr-icon {
+      font-size: 3vw;
+      margin: 0 0.4em;
+    }
+  }
 
   .editor-placeholder {
     font-size: 0.5em;
